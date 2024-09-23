4622bd1fac3ea9ef6ff94769f13c99af5e4f723d
Duong Minh Duy <duongminhduy@Duongs-MacBook-Air.local>
2024-09-23 23:10:19 +0700

    add api push notification by token

--- src/api/notification.ts
+++ src/api/notification.ts
@@ -1,8 +1,11 @@
 import { RawAxiosRequestHeaders } from 'axios';
+import { axiosInstanceJfw } from '../config/axios/axiosClient';
 import { IById, IListResponse } from '../models/interfaces';
 import {
   IGetNotificationRequestParams,
   INotification,
+  IPushDataMessageParams,
+  IPushDataMessagePayload,
   IUpdateAllNotificationsParams,
   IUpdateNotificationStatusParams,
 } from '../models/interfaces/notification';
@@ -11,6 +14,7 @@ import { get, put, remove } from '../utils/axiosHelper';
 const REST = 'notifications';
 const UPDATE_ALL_STATUS = 'update-all-status';
 const STATUS = 'update-status';
+const PUSH_DATA_MESSAGES = 'push-data-messages/by-token';
 
 export const getNotificationsAPI = async (
   params: IGetNotificationRequestParams,
@@ -60,6 +64,19 @@ export const updateAllNotificationsAPI = async (
 ) => {
   const url = `${REST}/${UPDATE_ALL_STATUS}`;
   const response = await get(url, { params }, userHeaders);
+
+  return response.data;
+};
+
+export const pushDataMessagesAPI = async (
+  params: IPushDataMessageParams,
+  payload: IPushDataMessagePayload,
+): Promise<boolean> => {
+  const url = `${REST}/${PUSH_DATA_MESSAGES}`;
+
+  const response = await axiosInstanceJfw.post(url, payload, {
+    params,
+  });
 
   return response.data;
 };
