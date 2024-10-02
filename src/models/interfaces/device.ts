432e36503af8c0289a53304f635c0ef0f09db2e7
Duong Minh Duy <duongminhduy@Duongs-MacBook-Air.local>
2024-10-02 16:09:20 +0700

    update IDevice

--- src/models/interfaces/device.ts
+++ src/models/interfaces/device.ts
@@ -1,15 +1,29 @@
 import { DateType, IdType } from '../types';
 import { IUser } from './user';
 
-// ADD
 export interface IDevice {
-  id: IdType;
-  name: string;
+  appVersionNumber: string;
   code: string;
+  createdDate: DateType;
+  iccid: string | null;
+  id: IdType;
+  imei: string | null;
+  imsi: string | null;
+  isDefault: boolean;
+  isMobileApp: boolean;
+  modifiedDate: DateType;
+  name: string;
   osDevice: string;
+  phoneNumber: string | null;
+  session: string;
+  simCardInfo: string | null;
+  status: number;
+  tags: string | null;
+  testMode: boolean;
+  token: string | null;
+  type: number;
   userId: IdType;
   user?: IUser;
-  createdDate: DateType;
 }
 export interface ICheckUserAccessParams {
   userId: string;
