e3f672e8813e3f8f72b2e6286f872ec4f75955e3
Kenneth <dao.khoi@1bytesoftware.com>
2025-04-10 16:30:24 +0700

    #feature: Update notification object following docs in developers.jframework.io

--- src/models/notification/api.ts
+++ src/models/notification/api.ts
@@ -1,33 +1,202 @@
 import { AxiosRequestConfig } from 'axios';
-import { HttpResponseList } from '../../core';
+import { HttpResponse, HttpResponseList } from '../../core';
 import { jfwAxios } from '../../core/client/client';
 import { generatePath } from '../../utils/path';
 import { IdType } from '../base';
 import { NOTIFICATION_PATH } from './paths';
-import { INotification, IQueryNotificationParams } from './types';
+import {
+    IGetNotificationsByUserAuthorizedParams,
+    INotification,
+    IPushNotificationDataMessageByDevicesParams,
+    IPushNotificationDataMessageByGivenDeviceCodeParams,
+    IPushNotificationDataMessageByTokensParams,
+    IPushNotificationMessageForTokensParams,
+    IPushNotificationResponse,
+    IUpdateStatusNotificationParams,
+} from './types';
 
 /**
- * List all notifications by the user authorized.
+ * Deletes tracking notification with id.
+ *
+ * @see {@link }
  */
-export const queryNotificationAPI = async (
-    params: IQueryNotificationParams,
+export const deleteNotificationAPI = async (
+    trackingNotificationId: IdType,
     config?: AxiosRequestConfig,
-): Promise<HttpResponseList<INotification>> => {
-    const url = NOTIFICATION_PATH.QUERY;
-    const response = await jfwAxios.get(url, { ...config, params });
+) => {
+    const url = generatePath(NOTIFICATION_PATH.DELETE_BY_ID, {
+        trackingNotificationId,
+    });
+
+    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
 
     return response.data;
 };
 
 /**
- * Updates status of the notification
+ * Get notifications by the user authorized.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/get-notifications-by-the-user-authorized}
  */
-export const updateNotificationStatusAPI = async (
+export const getNotificationByUserAuthorizedAPI = async (
+    params?: IGetNotificationsByUserAuthorizedParams,
+    config?: AxiosRequestConfig,
+): Promise<HttpResponseList<INotification>> => {
+    const url = NOTIFICATION_PATH.GET_NOTIFICATION_BY_USER_AUTHORIZED;
+    const response = await jfwAxios.get<HttpResponseList<INotification>>(url, {
+        params,
+        ...config,
+    });
+
+    return response.data;
+};
+
+/**
+ * Marks the tracking notification as read.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/mark-as-read}
+ */
+export const markNotificationAsReadAPI = async (
+    trackingNotificationId: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(NOTIFICATION_PATH.MARK_AS_READ, {
+        trackingNotificationId,
+    });
+
+    const response = await jfwAxios.post<HttpResponse<boolean>>(
+        url,
+        null,
+        config,
+    );
+    return response.data;
+};
+
+/**
+ * Push the notification with the given title and body to the given device tokens.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/push-notification-message-for-device-token}
+ */
+export const pushNotificationMessageForTokensAPI = async (
+    params: IPushNotificationMessageForTokensParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = NOTIFICATION_PATH.PUSH_NOTIFICATION_MESSAGE_FOR_TOKENS;
+
+    const response = await jfwAxios.post<
+        HttpResponse<IPushNotificationResponse>
+    >(url, null, { ...params, ...config });
+
+    return response.data;
+};
+
+/**
+ * Push notification data message by the given device codes
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/push-notification-data-messae-by-the-given-device-codes}
+ */
+export const pushNotificationDataMessageByGivenDeviceCodesAPI = async (
+    params: IPushNotificationDataMessageByGivenDeviceCodeParams,
+    data: Object,
+    config?: AxiosRequestConfig,
+) => {
+    const url =
+        NOTIFICATION_PATH.PUSH_NOTIFICATION_DATA_MESSAGE_BY_GIVEN_DEVICE_CODES;
+
+    const response = await jfwAxios.post<
+        HttpResponse<IPushNotificationResponse>
+    >(url, data, { ...params, ...config });
+
+    return response.data;
+};
+
+/**
+ * Push notification with data messages by the given device tokens.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/push-notification-data-message-by-tokens}
+ */
+export const pushNotificationDataMessageByTokensAPI = async (
+    params: IPushNotificationDataMessageByTokensParams,
+    data: Object,
+    config?: AxiosRequestConfig,
+) => {
+    const url = NOTIFICATION_PATH.PUSH_NOTIFICATION_DATA_MESSAGE_BY_TOKENS;
+
+    const response = await jfwAxios.post<
+        HttpResponse<IPushNotificationResponse>
+    >(url, data, { ...params, ...config });
+
+    return response.data;
+};
+
+/**
+ * Push notification with data messages by the given device ids.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/push-notification-data-message-by-devices}
+ */
+export const pushNotificationDataMessageByDevicesAPI = async (
+    params: IPushNotificationDataMessageByDevicesParams,
+    data: Object,
+    config?: AxiosRequestConfig,
+) => {
+    const url = NOTIFICATION_PATH.PUSH_NOTIFICATION_DATA_MESSAGE_BY_DEVICES;
+
+    const response = await jfwAxios.post<
+        HttpResponse<IPushNotificationResponse>
+    >(url, data, { ...params, ...config });
+
+    return response.data;
+};
+
+/**
+ * Update all status of the tracking notification by user authorized.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/updates-all-notification}
+ */
+export const updateAllNotificationAPI = async (
+    status: string,
+    config?: AxiosRequestConfig,
+) => {
+    const url = NOTIFICATION_PATH.UPDATE_ALL_NOTIFICATION;
+    const response = await jfwAxios.put(url, null, {
+        params: {
+            status,
+        },
+        ...config,
+    });
+
+    return response.data;
+};
+
+/**
+ * Updates status of the tracking notification.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/updates-status-notifications}
+ */
+export const updateStatusNotificationsAPI = async (
+    params: IUpdateStatusNotificationParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = NOTIFICATION_PATH.UPDATE_STATUS_NOTIFICATIONS;
+    const response = await jfwAxios.put(url, null, {
+        params,
+        ...config,
+    });
+
+    return response.data;
+};
+
+/**
+ * Updates status of the tracking notification.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/notifications/updates-status-of-a-notification}
+ */
+export const updateStatusOfNotificationAPI = async (
     trackingNotificationId: IdType,
     status: string,
     config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(NOTIFICATION_PATH.UPDATE_STATUS, {
+    const url = generatePath(NOTIFICATION_PATH.UPDATE_STATUS_OF_NOTIFICATION, {
         trackingNotificationId,
     });
 
@@ -38,48 +207,4 @@ export const updateNotificationStatusAPI = async (
         },
     });
     return response.data;
-};
-
-/**
- * Deletes tracking notification with id.
- */
-export const deleteNotificationAPI = async (
-    trackingNotificationId: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(NOTIFICATION_PATH.DELETE_BY_ID, {
-        trackingNotificationId,
-    });
-
-    const response = await jfwAxios.delete(url, config);
-    return response.data;
-};
-
-/**
- * Updates status of the tracking notification.
- */
-export const updateStatusAllNotificationAPI = async (
-    status: string,
-    config?: AxiosRequestConfig,
-) => {
-    const url = NOTIFICATION_PATH.UPDATE_ALL_STATUS;
-    const response = await jfwAxios.put(
-        url,
-        {
-            params: {
-                status,
-            },
-        },
-        config,
-    );
-
-    return response.data;
-};
-
-/**
- * Push notification data messages by the given device codes.
- * @feature Will make in feature
- */
-export const pushDataMessageAPI = async () => {
-    const url = NOTIFICATION_PATH.PUSH_DATA_MESSAGE;
 };
--- src/models/notification/constants.ts
+++ src/models/notification/constants.ts
@@ -1,5 +1,12 @@
-export const NOTIFICATION_STATUS = {
-    unread: 'Unread',
-    read: 'ReadEmailNotification',
-    takenAction: 'ReadEmailNotificationAndClickedTakenAction',
-};
+// export const NOTIFICATION_STATUS = {
+//     unread: 'Unread',
+//     read: 'ReadEmailNotification',
+//     takenAction: 'ReadEmailNotificationAndClickedTakenAction',
+// };
+
+export enum NotificationStatus {
+    Unread = 'Unread',
+    ReadEmailNotification = 'ReadEmailNotification',
+    ReadEmailNotificationButNotYetTakenAction = 'ReadEmailNotificationButNotYetTakenAction',
+    ReadEmailNotificationAndClickedTakenAction = 'ReadEmailNotificationAndClickedTakenAction',
+}
