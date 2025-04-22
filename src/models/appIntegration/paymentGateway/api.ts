1f1b26752ae1f29b8bf573e88afadb0da777bd77
Kenneth <dao.khoi@1bytesoftware.com>
2025-04-22 14:21:30 +0700

    #feature: Add app integration payment gateway api

--- src/models/appIntegration/paymentGateway/api.ts
+++ src/models/appIntegration/paymentGateway/api.ts
@@ -1,81 +1,143 @@
-import { HttpResponseList } from '../../../core';
+import { AxiosRequestConfig } from 'axios';
+import { HttpResponse } from '../../../core';
 import { jfwAxios } from '../../../core/client/client';
 import { generatePath } from '../../../utils/path';
 import { IdType } from '../../base';
-import { INTEGRATION_PAYMENT_GATEWAY_PATH } from './paths';
+import { APP_INTEGRATION_PAYMENT_GATEWAY_PATH } from './paths';
 import {
-    ICreateIntegrationPaymentGatewayParams,
-    IIntegrationPaymentGateway,
-    IQueryIntegrationPaymentGatewayParams,
-    IUpdateIntegrationPaymentGatewayParams,
+    IAppIntegrationPaymentGateway,
+    ICreateAppIntegrationPaymentGatewayData,
+    IGetAppIntegrationPaymentGatewaysWithBrandParams,
+    ITestingCreateCheckoutLinkData,
+    IUpdateAppIntegrationPaymentGatewayData,
 } from './types';
 
 /**
- * Gets the list of the app integration payment gateways by the current brand.
+ * Create an app integration payment gateway
+ *
+ * @link {@see https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/creates-an-app-integration-payment-gateway}
  */
-export const queryIntegrationPaymentGatewayAPI = async (
-    params?: IQueryIntegrationPaymentGatewayParams,
-): Promise<HttpResponseList<IIntegrationPaymentGateway>> => {
-    const url = INTEGRATION_PAYMENT_GATEWAY_PATH.QUERY;
+export const createAppIntegrationPaymentGatewayAPI = async (
+    data: ICreateAppIntegrationPaymentGatewayData,
+    config?: AxiosRequestConfig,
+) => {
+    const url =
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.CREATE_APP_INTEGRATION_PAYMENT_GATEWAY;
 
-    const response = await jfwAxios.get(url, {
-        params,
-    });
+    const response = await jfwAxios.post<
+        HttpResponse<IAppIntegrationPaymentGateway>
+    >(url, data, config);
+
+    return response.data;
+};
+
+/**
+ * Deletes an app integration payment gateway by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/deletes-an-app-integration-payment-gateway}
+ */
+export const deleteAppIntegrationPaymentGatewayAPI = async (
+    appIntegrationPaymentGatewayId: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.DELETE_APP_INTEGRATION_PAYMENT_GATEWAY,
+        {
+            id: appIntegrationPaymentGatewayId,
+        },
+    );
+    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
 
     return response.data;
 };
 
 /**
  * Gets a app integration payment gateway by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/gets-an-app-integration-payment-gateway}
  */
-export const getIntegrationPaymentGatewayByIdAPI = async (
+export const getAppIntegrationPaymentGatewayAPI = async (
     appIntegrationPaymentGatewayId: IdType,
-): Promise<IIntegrationPaymentGateway> => {
-    const url = generatePath(INTEGRATION_PAYMENT_GATEWAY_PATH.GET_BY_ID, {
-        id: appIntegrationPaymentGatewayId,
-    });
-    const response = await jfwAxios.get(url);
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.GET_APP_INTEGRATION_PAYMENT_GATEWAY,
+        {
+            id: appIntegrationPaymentGatewayId,
+        },
+    );
+    const response = await jfwAxios.get<
+        HttpResponse<IAppIntegrationPaymentGateway>
+    >(url, config);
 
     return response.data;
 };
 
 /**
- * Creates a new app integration payment gateway.
+ * Gets the list of the app integration payment gateways by the current brand.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/gets-app-integration-payment-gateways-with-brand}
  */
-export const createIntegrationPaymentGatewayAPI = async (
-    params: ICreateIntegrationPaymentGatewayParams,
+export const getAppIntegrationPaymentGatewaysWithBrandAPI = async (
+    params?: IGetAppIntegrationPaymentGatewaysWithBrandParams,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = INTEGRATION_PAYMENT_GATEWAY_PATH.CREATE;
-    const response = await jfwAxios.post(url, params);
+    const url =
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.GET_APP_INTEGRATION_PAYMENT_GATEWAYS_WITH_BRAND;
+
+    const response = await jfwAxios.get<
+        HttpResponse<IAppIntegrationPaymentGateway[]>
+    >(url, {
+        params,
+        ...config,
+    });
 
     return response.data;
 };
 
 /**
- * Updates a app integration payment gateway by the given id.
+ * Testing for create a checkout link
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/test-create-checkout-link}
  */
-export const updateIntegrationPaymentGatewayByIdAPI = async (
-    appIntegrationPaymentGatewayId: IdType,
-    payload: IUpdateIntegrationPaymentGatewayParams,
+export const testingCreateCheckoutLinkAPI = async (
+    appIntegrationId: IdType,
+    data: ITestingCreateCheckoutLinkData,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(INTEGRATION_PAYMENT_GATEWAY_PATH.UPDATE_BY_ID, {
-        id: appIntegrationPaymentGatewayId,
-    });
-    const response = await jfwAxios.put(url, payload);
+    const url = generatePath(
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.TESTING_FOR_CREATE_CHECKOUT_LINK,
+        {
+            appIntegrationId,
+        },
+    );
+
+    const response = await jfwAxios.post<
+        HttpResponse<string>
+    >(url, data, config);
 
     return response.data;
 };
 
 /**
- * Deletes a app integration payment gateway by the given id.
+ * Updates an app integration payment gateway by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/payment-gateway/updates-an-app-integration-payment-gateway-copy}
  */
-export const deleteIntegrationPaymentGatewayAPI = async (
+export const updateAppIntegrationPaymentGatewayAPI = async (
     appIntegrationPaymentGatewayId: IdType,
+    data: IUpdateAppIntegrationPaymentGatewayData,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(INTEGRATION_PAYMENT_GATEWAY_PATH.DELETE_BY_ID, {
-        id: appIntegrationPaymentGatewayId,
-    });
-    const response = await jfwAxios.delete(url);
+    const url = generatePath(
+        APP_INTEGRATION_PAYMENT_GATEWAY_PATH.UPDATE_APP_INTEGRATION_PAYMENT_GATEWAY,
+        {
+            id: appIntegrationPaymentGatewayId,
+        },
+    );
+    const response = await jfwAxios.put<
+        HttpResponse<IAppIntegrationPaymentGateway>
+    >(url, data, config);
 
     return response.data;
 };
--- src/models/appIntegration/paymentGateway/types.ts
+++ src/models/appIntegration/paymentGateway/types.ts
@@ -1,31 +1,188 @@
 import { DateType, IdType } from '../../base';
+import { IAppIntegration } from '../types';
+import { AppIntegrationPaymentGatewayStatus } from './constants';
 
-export interface IIntegrationPaymentGateway {
+export interface IAppIntegrationPaymentGateway {
     id: IdType;
+    appIntegration: IAppIntegration;
     name: string;
-    description?: string;
-    smtpHost: string;
-    smtpPort: number;
-    smtpUsername: string;
-    useTls: boolean;
+    ipnListenerLink: string;
+    cancelLinkWithoutLogin?: string | null;
+    cancelLink: string;
+    returnLink: string;
+    publicKey: string;
+    privateKey: string;
+    description?: string | null;
+    notes?: string | null;
     isDefault: boolean;
+    testMode?: boolean | null;
+    status: AppIntegrationPaymentGatewayStatus;
     createdDate: DateType;
 }
 
-//#region API types
-export interface IQueryIntegrationPaymentGatewayParams {}
+export interface IAppIntegrationPaymentGatewayProduct {
+    /**
+     * The name of the item.
+     */
+    name?: string | null;
 
-export interface ICreateIntegrationPaymentGatewayParams {
-    name: string;
-    description?: string;
-    smtpHost: string;
-    smtpPort: number;
-    smtpUsername: string;
-    useTls?: boolean;
-    isDefault?: boolean;
+    /**
+     * The description of the item.
+     */
+    description?: string | null;
+
+    /**
+     * The currency code. PayPal supports the following currencies for use:
+     * AUD, BRL, CAD, CNY, CZK, DKK, EUR, HKD, HUF, ILS, JPY, MYR, MXN,
+     * TWD, NZD, NOK, PHP, PLN, GBP, SGD, SEK, CHF, THB, USD.
+     */
+    currencyCode?: string | null;
+
+    /**
+     * The code of the item.
+     */
+    code?: string | null;
+
+    /**
+     * The quantity of the item. By default is 1.
+     */
+    quantity: number;
+
+    /**
+     * The amount of the item.
+     */
+    amount: number;
+
+    /**
+     * The return url.
+     */
+    returnUrl?: string | null;
+
+    /**
+     * The cancel url.
+     */
+    cancelUrl?: string | null;
 }
 
-export type IUpdateIntegrationPaymentGatewayParams =
-    ICreateIntegrationPaymentGatewayParams;
+//#region API types
+export interface IGetAppIntegrationPaymentGatewaysWithBrandParams {}
+
+export interface ICreateAppIntegrationPaymentGatewayData {
+    /**
+     * The app integration id of the payment gateway.
+     */
+    appIntegrationId: string;
+
+    /**
+     * The name of the payment gateway integration.
+     */
+    name?: string | null;
+
+    /**
+     * The IPN (Instant Payment Notification) listener link of the payment gateway.
+     * This endpoint will receive payment notifications from the gateway.
+     * For PayPal: This is your IPN listener URL for payment status updates.
+     * For Stripe: This would be your webhook URL for payment events.
+     */
+    ipnListenerLink: string;
+
+    /**
+     * The payment link of the payment gateway. This is used when unauthenticated users cancel payment and need to be redirected.
+     * For PayPal/Stripe: Fallback URL when session is lost or user isn't logged in.
+     */
+    cancelLinkWithoutLogin?: string | null;
+
+    /**
+     * The cancel link of the payment gateway. Users will be redirected here when they cancel a payment.
+     * For PayPal: Set as the cancel_url in PayPal checkout.
+     * For Stripe: Used as cancel_url in Stripe Checkout sessions.
+     */
+    cancelLink: string;
+
+    /**
+     * The return link of the payment gateway. Users will be redirected here after successful payment.
+     * For PayPal: Set as the return_url in PayPal checkout.
+     * For Stripe: Used as success_url in Stripe Checkout sessions.
+     */
+    returnLink: string;
+
+    /**
+     * The public key of the payment gateway.
+     * For PayPal: Client ID from your PayPal app credentials.
+     * For Stripe: Publishable key from your Stripe dashboard.
+     */
+    publicKey: string;
+
+    /**
+     * The private key of the payment gateway.
+     * For PayPal: Secret from your PayPal app credentials.
+     * For Stripe: Secret key from your Stripe dashboard.
+     */
+    privateKey: string;
+
+    /**
+     * Description of the payment gateway.
+     * For PayPal/Stripe: Optional description that appears in your dashboard.
+     */
+    description?: string | null;
+
+    /**
+     * The notes of the payment gateway.
+     * For PayPal/Stripe: Internal notes about this integration.
+     */
+    notes?: string | null;
+
+    /**
+     * Flag to indicate if the Payment gateway integration is the default. By default, it is set to false.
+     */
+    isDefault?: boolean | null;
+
+    /**
+     * Available options: 0 - Inactive, 1 - Active
+     */
+    status?: AppIntegrationPaymentGatewayStatus;
+
+    /**
+     * Flag to indicate if the payment gateway is in test mode.
+     * When true, transactions will use the provider's sandbox environment.
+     * For PayPal: Uses https://api.sandbox.paypal.com instead of production.
+     * For Stripe: Uses test mode keys and endpoints.
+     * Default is false (production mode).
+     */
+    testMode?: boolean | null;
+}
+
+export type IUpdateAppIntegrationPaymentGatewayData =
+    ICreateAppIntegrationPaymentGatewayData;
+
+export interface ITestingCreateCheckoutLinkData {
+    /**
+     * This class represents the Checkout Payment Request PayPal Data Transfer Object.
+     */
+    product: IAppIntegrationPaymentGatewayProduct;
+
+    /**
+     * The public key of the payment gateway used for authentication.
+     * For PayPal: Client ID from your PayPal app credentials (used with OAuth 2.0).
+     * For Stripe: Publishable key from your Stripe dashboard (used client-side).
+     */
+    publicKey: string;
+
+    /**
+     * The private key of the payment gateway used for secure server-side operations.
+     * For PayPal: Secret from your PayPal app credentials.
+     * For Stripe: Secret key from your Stripe dashboard (used for server-side API calls).
+     * Note: This should never be exposed to client-side code.
+     */
+    privateKey: string;
+
+    /**
+     * The display name shown to customers during checkout.
+     * For PayPal: Appears as the merchant name during PayPal checkout flow.
+     * For Stripe: Displayed as the company name on Stripe Checkout pages.
+     * Maximum length is typically 127 characters for most payment providers.
+     */
+    displayName: string;
+}
 
 //#endregion
