70c3fa8dd45a8b2ba3f10906610890137449a948
Kenneth <dao.khoi@1bytesoftware.com>
2025-04-22 14:21:45 +0700

    #feature: Add app integration SMTP api

--- src/models/appIntegration/smtp/api.ts
+++ src/models/appIntegration/smtp/api.ts
@@ -1,81 +1,143 @@
-import { HttpResponseList } from '../../../core';
+import { AxiosRequestConfig } from 'axios';
+import { HttpResponse } from '../../../core';
 import { jfwAxios } from '../../../core/client/client';
 import { generatePath } from '../../../utils/path';
 import { IdType } from '../../base';
-import { INTEGRATION_SMTP_PATH } from './paths';
+import { APP_INTEGRATION_SMTP_PATH } from './paths';
 import {
-    ICreateIntegrationSMTPParams,
-    IIntegrationSMTP,
-    IQueryIntegrationSMTPParams,
-    IUpdateIntegrationSMTPParams,
+    IAppIntegrationSMTP,
+    ICreateAppIntegrationSMTPData,
+    IGetAppIntegrationSMTPsParams,
+    ISendTestEmailData,
+    IUpdateAppIntegrationSMTPData,
 } from './types';
 
 /**
- * Gets all app integration SMTP.
- */
-export const queryIntegrationSMTPAPI = async (
-    params?: IQueryIntegrationSMTPParams,
-): Promise<HttpResponseList<IIntegrationSMTP>> => {
-    const url = INTEGRATION_SMTP_PATH.QUERY;
-
-    const response = await jfwAxios.get(url, {
-        params,
-    });
-
-    return response.data;
-};
-
-/**
- * Gets the app integration SMTP by the given id.
- */
-export const getIntegrationSMTPByIdAPI = async (
-    appIntegrationSMTPId: IdType,
-): Promise<IIntegrationSMTP> => {
-    const url = generatePath(INTEGRATION_SMTP_PATH.GET_BY_ID, {
-        id: appIntegrationSMTPId,
-    });
-    const response = await jfwAxios.get(url);
-
-    return response.data;
-};
-
-/**
  * Creates a new app integration SMTP.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/smtp/creates-an-app-integration-smtp}
  */
-export const createIntegrationSMTPAPI = async (
-    params: ICreateIntegrationSMTPParams,
+export const createAppIntegrationSMTPAPI = async (
+    data: ICreateAppIntegrationSMTPData,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = INTEGRATION_SMTP_PATH.CREATE;
-    const response = await jfwAxios.post(url, params);
-
-    return response.data;
-};
-
-/**
- * Updates the app integration SMTP by the given id.
- */
-export const updateIntegrationSMTPByIdAPI = async (
-    appIntegrationSMTPId: IdType,
-    payload: IUpdateIntegrationSMTPParams,
-) => {
-    const url = generatePath(INTEGRATION_SMTP_PATH.UPDATE_BY_ID, {
-        id: appIntegrationSMTPId,
-    });
-    const response = await jfwAxios.put(url, payload);
+    const url = APP_INTEGRATION_SMTP_PATH.CREATE_APP_INTEGRATION_SMTP;
+    const response = await jfwAxios.post<HttpResponse<IAppIntegrationSMTP>>(
+        url,
+        data,
+        config,
+    );
 
     return response.data;
 };
 
 /**
  * Deletes the app integration SMTP by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/smtp/deletes-an-app-integration-smtp}
  */
-export const deleteIntegrationSMTPAPI = async (
-    appIntegrationSMTPId: IdType,
+export const deleteAppIntegrationSMTPAPI = async (
+    id: IdType,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(INTEGRATION_SMTP_PATH.DELETE_BY_ID, {
-        id: appIntegrationSMTPId,
-    });
-    const response = await jfwAxios.delete(url);
+    const url = generatePath(
+        APP_INTEGRATION_SMTP_PATH.DELETE_APP_INTEGRATION_SMTP,
+        {
+            id,
+        },
+    );
+    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * Gets the app integration SMTP by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/smtp/gets-an-app-integration-smtp}
+ */
+export const getAppIntegrationSMTPAPI = async (
+    id: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(
+        APP_INTEGRATION_SMTP_PATH.GET_APP_INTEGRATION_SMTP,
+        {
+            id,
+        },
+    );
+
+    const response = await jfwAxios.get<HttpResponse<IAppIntegrationSMTP>>(
+        url,
+        config,
+    );
+
+    return response.data;
+};
+
+/**
+ * Gets the app integration SMTP by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/smtp/gets-an-app-integration-smtp-with-brand}
+ */
+export const getAppIntegrationSMTPsWithBrandAPI = async (
+    params?: IGetAppIntegrationSMTPsParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = APP_INTEGRATION_SMTP_PATH.GET_APP_INTEGRATION_SMTPS_WITH_BRAND;
+
+    const response = await jfwAxios.get<HttpResponse<IAppIntegrationSMTP[]>>(
+        url,
+        {
+            ...config,
+            params,
+        },
+    );
+
+    return response.data;
+};
+
+/**
+ * Testing for send SMS base on user's setting.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/sms/send-sms-test-message}
+ */
+export const sendTestEmailAPI = async (
+    data: ISendTestEmailData,
+    config?: AxiosRequestConfig,
+) => {
+    const url = APP_INTEGRATION_SMTP_PATH.SEND_TEST_EMAIL;
+
+    const response = await jfwAxios.post<HttpResponse<boolean>>(
+        url,
+        data,
+        config,
+    );
+
+    return response.data;
+};
+
+/**
+ * Updates the app integration SMTP by the given id.
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/app-integrations/smtp/update-an-app-integration-smtp}
+ */
+export const updateAppIntegrationSMTPAPI = async (
+    id: IdType,
+    data: IUpdateAppIntegrationSMTPData,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(
+        APP_INTEGRATION_SMTP_PATH.UPDATE_APP_INTEGRATION_SMTP,
+        {
+            id,
+        },
+    );
+    const response = await jfwAxios.put<HttpResponse<IAppIntegrationSMTP>>(
+        url,
+        data,
+        config,
+    );
 
     return response.data;
 };
--- src/models/appIntegration/smtp/types.ts
+++ src/models/appIntegration/smtp/types.ts
@@ -1,32 +1,124 @@
 import { DateType, IdType } from '../../base';
+import { IAppIntegration } from '../types';
+import { AppIntegrationSMTPStatus } from './constants';
 
-export interface IIntegrationSMTP {
+export interface IAppIntegrationSMTP {
     id: IdType;
+    appIntegration: IAppIntegration;
     name: string;
-    description?: string;
-    smtpHost: string;
-    smtpPort: number;
-    smtpUsername: string;
-    useTls: boolean;
-    isDefault: boolean;
-    createdDate: DateType;
-}
-
-//#region API types
-export interface IQueryIntegrationSMTPParams {}
-
-export interface ICreateIntegrationSMTPParams {
-    type?: number | null;
-    name?: string | null;
     host: string;
     port: number;
     username: string;
     password: string;
+    useTls: boolean;
+    isDefault: boolean;
+    description?: string | null;
+    status: AppIntegrationSMTPStatus;
+    createdDate: DateType;
+}
+
+//#region API types
+export interface IGetAppIntegrationSMTPsParams {}
+
+export interface ICreateAppIntegrationSMTPData {
+    /**
+     * The type of the SMTP integration.
+     */
+    appIntegrationId: string;
+
+    /**
+     * The name of the SMTP integration.
+     */
+    name?: string | null;
+
+    /**
+     * The host of the SMTP integration.
+     */
+    host: string;
+
+    /**
+     * The port of the SMTP integration.
+     */
+    port: number;
+
+    /**
+     * The username of the SMTP integration.
+     */
+    username: string;
+
+    /**
+     * The password of the SMTP integration.
+     */
+    password: string;
+
+    /**
+     * Flag to indicate if the SMTP integration uses TLS. By default, it is set to false.
+     */
     useTls?: boolean | null;
+
+    /**
+     * Flag to indicate if the SMTP integration is the default. By default, it is set to false.
+     */
     isDefault?: boolean | null;
+
+    /**
+     * Available options: 0 - Inactive, 1 - Active
+     */
+    status?: AppIntegrationSMTPStatus;
+
+    /**
+     * The description of the SMTP integration.
+     */
     description?: string | null;
 }
 
-export type IUpdateIntegrationSMTPParams = ICreateIntegrationSMTPParams;
+export type IUpdateAppIntegrationSMTPData = ICreateAppIntegrationSMTPData;
+
+export interface ISendTestEmailData {
+    /**
+     * The SMTP server hostname or IP (e.g., smtp.gmail.com).
+     */
+    host: string;
+
+    /**
+     * The port number for the SMTP server (typically 587 or 465).
+     */
+    port: number;
+
+    /**
+     * The username/email for SMTP authentication.
+     */
+    username: string;
+
+    /**
+     * The password for SMTP authentication.
+     */
+    password: string;
+
+    /**
+     * Whether to enable SSL for SMTP connection.
+     */
+    useTls: boolean;
+
+    /**
+     * The sender email address.
+     */
+    emailAddressFrom: string;
+
+    /**
+     * The recipient email address.
+     */
+    emailAddressTo: string;
+
+    /**
+     * The subject of the test email.
+     */
+    subject: string;
+
+    /**
+     * The body content of the test email.
+     */
+    body: string;
+}
 
 //#endregion
