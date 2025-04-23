0fb2edae9b1b966ec16db2d2813a2d59528f1169
Kenneth <dao.khoi@1bytesoftware.com>
2025-04-23 22:20:11 +0700

    #hotfix: city api interface

--- src/models/city/api.ts
+++ src/models/city/api.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '../../core';
+import { HttpResponse, HttpResponseList } from '../../core';
 import { jfwAxios } from '../../core/client/client';
 import { CITY_PATH } from './paths';
 import { ICity, IGetCitiesParams } from './types';
@@ -10,7 +10,7 @@ import { ICity, IGetCitiesParams } from './types';
  */
 export const getCitiesAPI = async (params?: IGetCitiesParams) => {
     const url = CITY_PATH.QUERY;
-    const response = await jfwAxios.get<HttpResponse<ICity[]>>(url, {
+    const response = await jfwAxios.get<HttpResponseList<ICity>>(url, {
         params,
     });
 
