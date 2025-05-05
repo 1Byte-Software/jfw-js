66c01c8059601f831d22ad18b50d46e4746dbafa
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-05 14:41:31 +0700

    #feature: Update tracking acitivity api

--- src/models/trackingActivity/types.ts
+++ src/models/trackingActivity/types.ts
@@ -22,21 +22,38 @@ export interface ITrackingActivity {
 }
 //#region API types
 export interface IGetTrackingActivitiesParams extends IPageable, ISortable {
-    keywords?: string;
-    browserName?: string;
-    browserVersion?: string;
-    description?: string;
-    deviceId?: null;
-    ipAddress?: string;
-    latitude?: number;
-    location?: string;
-    longitude?: number;
-    os?: string;
-    requestFromMobile?: boolean;
-    eventId?: IdType;
-    url?: string;
-    userAgent?: string;
+    /** Filter by user id */
     userId?: IdType;
+    /** Filter by device id */
+    deviceId?: null;
+    /** Filter by tracking event id */
+    eventId?: IdType;
+    /** Filter by the url requested */
+    url?: string;
+    /** Filter by the ip address */
+    ipAddress?: string;
+    /** Filter by the os */
+    os?: string;
+    /** Filter by the browser name */
+    browserName?: string;
+    /** Filter by the browser version */
+    browserVersion?: string;
+    /** Filter by the request from mobile */
+    requestFromMobile?: boolean;
+    /** Filter by the user agent */
+    userAgent?: string;
+    /** Filter by the location */
+    location?: string;
+    /** Filter by the latitude */
+    latitude?: number;
+    /** Filter by the longitude */
+    longitude?: number;
+    /** Filter by the description */
+    description?: string;
+    /** Filter by the tags */
+    tags?: string;
+    /** Filter by the keywords */
+    keywords?: string;
 }
 
 export interface ICreateTrackingActivityParams {
