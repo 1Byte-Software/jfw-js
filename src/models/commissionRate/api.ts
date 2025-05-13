281aaf54acc8a10f2241105ec47ad62574e2a0e9
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-13 14:12:13 +0700

    feature: update commission rate api following document

--- src/models/commissionRate/api.ts
+++ src/models/commissionRate/api.ts
@@ -1,95 +1,145 @@
+import { AxiosRequestConfig } from 'axios';
+import { HttpResponse } from '../../core';
 import { jfwAxios } from '../../core/client/client';
 import { generatePath } from '../../utils/path';
 import { IdType } from '../base';
 import { COMMISSION_RATE_PATH } from './paths';
 import {
     ICommissionRate,
-    ICreateCommissionRateParams,
+    ICreateCommissionRateData,
+    IGetCommissionRatesParams,
     IGetDiscountCommissionDiscountParams,
-    IQueryCommissionRatesParams,
-    IUpdateCommissionRateParams,
+    IUpdateCommissionRateData,
 } from './types';
 
 /**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
+ * Create a new commission rate.
+ *
+ * @param data - The data for creating an commission rate.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/create-a-commission-rate}
  */
-export const queryCommissionRateAPI = async (
-    params?: IQueryCommissionRatesParams,
-): Promise<ICommissionRate[]> => {
-    const url = COMMISSION_RATE_PATH.QUERY;
+export const createCommissionRateAPI = async (
+    data?: ICreateCommissionRateData,
+    config?: AxiosRequestConfig,
+) => {
+    const url = COMMISSION_RATE_PATH.CREATE_COMMISSION_RATE;
 
-    const response = await jfwAxios.get(url, {
-        params,
-    });
+    const response = await jfwAxios.post<HttpResponse<ICommissionRate>>(
+        url,
+        data,
+        config,
+    );
 
     return response.data;
 };
 
 /**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
+ * Delete a commission rate by id.
+ *
+ * @param id - The id for deleting a commission rate.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/delete-a-commission-rate}
+ */
+export const deleteCommissionRateAPI = async (
+    id: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(COMMISSION_RATE_PATH.DELETE_COMMISSION_RATE, {
+        id,
+    });
+
+    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * Gets a commission rate by id.
+ *
+ * @param id - The id of the commission rate.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/get-a-commission-rate}
+ */
+export const getCommissionRateAPI = async (
+    id: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(COMMISSION_RATE_PATH.GET_COMMISSION_RATE, {
+        id,
+    });
+    const response = await jfwAxios.get<HttpResponse<ICommissionRate>>(
+        url,
+        config,
+    );
+
+    return response.data;
+};
+
+/**
+ * Gets the discount value based on the quantity.
+ *
+ * @param params - The parameters for getting discount.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/get-discount-value}
  */
 export const getCommissionDiscountAPI = async (
     params?: IGetDiscountCommissionDiscountParams,
-): Promise<number> => {
-    const url = COMMISSION_RATE_PATH.GET_DISCOUNT;
+    config?: AxiosRequestConfig,
+) => {
+    const url = COMMISSION_RATE_PATH.GET_DISCOUNT_VALUE;
 
-    const response = await jfwAxios.get(url, {
+    const response = await jfwAxios.get<HttpResponse<number>>(url, {
         params,
+        ...config,
     });
 
     return response.data;
 };
 
 /**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
+ * List all commission rates.
+ *
+ * @param params - The parameters for getting all commission rates.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/list-commission-rates}
+ * #JFW-283
  */
-export const getCommissionRateByIdAPI = async (
-    commissionRateId: IdType,
-): Promise<ICommissionRate> => {
-    const url = generatePath(COMMISSION_RATE_PATH.GET_BY_ID, {
-        id: commissionRateId,
-    });
-    const response = await jfwAxios.get(url);
-
-    return response.data;
-};
-
-/**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
- */
-export const createCommissionRateAPI = async (
-    params?: ICreateCommissionRateParams,
+export const getCommissionRatesAPI = async (
+    params?: IGetCommissionRatesParams,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = COMMISSION_RATE_PATH.CREATE;
-    const response = await jfwAxios.post(url, params);
+    const url = COMMISSION_RATE_PATH.GET_COMMISSION_RATES;
+    const response = await jfwAxios.get<HttpResponse<ICommissionRate[]>>(url, {
+        params,
+        ...config
+    });
 
     return response.data;
 };
 
 /**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
+ * Update a commission rate.
+ *
+ * @param id - The id of the commission rate.
+ * @param data - The data for updating a commission rate.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/commission-rates/update-a-commission-rate}
  */
-export const updateCommissionRateByIdAPI = async (
-    commissionRateId: IdType,
-    params?: IUpdateCommissionRateParams,
+export const updateCommissionRateAPI = async (
+    id: IdType,
+    data?: IUpdateCommissionRateData,
+    config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(COMMISSION_RATE_PATH.UPDATE_BY_ID, {
-        id: commissionRateId,
-    });
-    const response = await jfwAxios.put(url, params);
-
-    return response.data;
-};
-
-/**
- * #JFW-83: Thiếu tài liệu api/conmission-rates
- */
-export const deleteCommissionRateByIdAPI = async (commissionRateId: IdType) => {
-    const url = generatePath(COMMISSION_RATE_PATH.DELETE_BY_ID, {
-        id: commissionRateId,
+    const url = generatePath(COMMISSION_RATE_PATH.UPDATE_COMMISSION_RATE, {
+        id,
     });
 
-    const response = await jfwAxios.delete(url);
+    const response = await jfwAxios.put<HttpResponse<boolean>>(
+        url,
+        data,
+        config,
+    );
 
     return response.data;
 };
