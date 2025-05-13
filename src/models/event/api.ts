a16436295414e5ea0e6b88490b2a590643603e95
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-13 14:14:36 +0700

    feature: add event api following document

--- src/models/event/api.ts
+++ src/models/event/api.ts
@@ -10,15 +10,16 @@ import {
     IEvent,
     IGetDefaultEmailTemplateParams,
     IGetDefaultPhoneTemplateParams,
-    IGetEmailTemplateEventsParams,
-    IGetEventsEarningWallet,
+    IGetEventsEarningWalletParams,
     IGetEventsParams,
-    IGetPhoneTemplateEventsParams,
+    IWalletEarningEvent,
 } from './types';
 
 /**
  * Get events
  *
+ * @param params - The parameters for getting events.
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/events/get-events}
  */
 export const getEventsAPI = async (
@@ -36,61 +37,64 @@ export const getEventsAPI = async (
 };
 
 /**
- * List all wallet earning events. By default, the type is Earning.
+ * Get events of the wallet earning. By default, the type is Earning.
  *
+ * @param params - The parameters for getting events earning wallet.
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/events/get-events-earning-wallet}
  */
 export const getEventsEarningWalletAPI = async (
-    params?: IGetEventsEarningWallet,
+    params?: IGetEventsEarningWalletParams,
     config?: AxiosRequestConfig,
 ) => {
     const url = EVENT_PATH.GET_EVENTS;
 
-    const response = await jfwAxios.get<HttpResponseList<IEvent>>(url, {
-        params,
-        ...config,
-    });
+    const response = await jfwAxios.get<HttpResponseList<IWalletEarningEvent>>(
+        url,
+        {
+            params,
+            ...config,
+        },
+    );
 
     return response.data;
 };
 
 /**
- * Wait document. Will update in feature.
+ * Get events that are associated with email templates.
+ *
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/events/get-events-email-templates}
  */
 export const getEmailTemplateEventsAPI = async (
-    params?: IGetEmailTemplateEventsParams,
     config?: AxiosRequestConfig,
 ) => {
     const url = EVENT_PATH.GET_EMAIL_TEMPLATE_EVENTS;
 
-    const response = await jfwAxios.get<HttpResponse<IEvent[]>>(url, {
-        params,
-        ...config,
-    });
+    const response = await jfwAxios.get<HttpResponse<IEvent[]>>(url, config);
 
     return response.data;
 };
 
 /**
- * Wait document. Will update in feature.
+ * Get events that are associated with phone templates.
+ *
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/events/get-events-phone-templates}
  */
 export const getPhoneTemplateEventsAPI = async (
-    params?: IGetPhoneTemplateEventsParams,
     config?: AxiosRequestConfig,
 ) => {
     const url = EVENT_PATH.GET_PHONE_TEMPLATE_EVENTS;
 
-    const response = await jfwAxios.get<HttpResponse<IEvent[]>>(url, {
-        params,
-        ...config,
-    });
+    const response = await jfwAxios.get<HttpResponse<IEvent[]>>(url, config);
 
     return response.data;
 };
 
 /**
- *
  * Wait document. Will update in feature.
+ * #JFW-298
  */
 export const getDefaultEmailTemplateAPI = async (
     eventId: IdType,
@@ -113,8 +117,8 @@ export const getDefaultEmailTemplateAPI = async (
 };
 
 /**
- *
  * Wait document. Will update in feature.
+ * #JFW-298F
  */
 export const getDefaultPhoneTemplatesAPI = async (
     eventId: IdType,
