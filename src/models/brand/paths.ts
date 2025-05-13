2f159f4c156e075d4d20b5fa80ac29de4043331a
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-13 14:11:41 +0700

    feature: update domain api following document

--- src/models/brand/paths.ts
+++ src/models/brand/paths.ts
@@ -7,7 +7,7 @@ export const BRAND_PATH = {
     GET_BY_URL: `v1/${REST}/by-url`,
 
     CREATE: `v1/${REST}`,
-    UPDATE_BY_ID: `${REST}/:id`,
+    UPDATE_BY_ID: `v1/${REST}/:id`,
 
     CHECK_DOMAIN_EXISTS: `v1/${REST}/check-domain-exists/:domainUrl`,
     GENERATE_DOMAIN: `v1/${REST}/generate-domain`,
