927a6fa2374ad93b30428f4d9abd02ae70c2dcf2
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-22 09:46:30 +0700

    feature: update brand email address function api by doc

--- src/models/brand/emailAddress/api.ts
+++ src/models/brand/emailAddress/api.ts
@@ -10,6 +10,7 @@ import { IBrandEmailAddress, IUpdateEmailAddressData } from './types';
  * Gets the brand's email by the given brand id.
  *
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/get-emails-with-a-brand}
+ * #JFW-361
  */
 export const getEmailsWithBrandAPI = async (
     brandId: IdType,
@@ -18,7 +19,6 @@ export const getEmailsWithBrandAPI = async (
     const url = generatePath(BRAND_EMAIL_ADDRESS_PATH.GET_EMAILS_WITH_BRAND, {
         id: brandId,
     });
-
     const response = await jfwAxios.get<HttpResponse<IBrandEmailAddress>>(
         url,
         config,
@@ -30,20 +30,23 @@ export const getEmailsWithBrandAPI = async (
 /**
  * Updates the brand's emails by the given brand id.
  *
+ * @param id - The id of the brand.
+ * @param data - The data for updating a brand's email.
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/update-a-email}
+ * #JFW-361
  */
 export const updateBrandEmailAddressAPI = async (
-    brandId: IdType,
+    id: IdType,
     data: IUpdateEmailAddressData,
     config?: AxiosRequestConfig,
 ) => {
     const url = generatePath(
         BRAND_EMAIL_ADDRESS_PATH.UPDATE_BRAND_EMAIL_ADDRESS,
         {
-            id: brandId,
+            id,
         },
     );
-
     const response = await jfwAxios.put<HttpResponse<IBrandEmailAddress>>(
         url,
         data,
