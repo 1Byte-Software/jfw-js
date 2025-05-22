0ebdeb347ed200d2e6eaa53cb7bed3ccbac51247
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-22 09:46:45 +0700

    feature: update brand function api by doc

--- src/models/brand/api.ts
+++ src/models/brand/api.ts
@@ -8,58 +8,22 @@ import {
     IBrand,
     ICreateBrandData,
     IGeneratedDomain,
-    IGetQueryBrandParams,
+    IGetBrandsParams,
     IUpdateBrandData,
 } from './types';
 
 /**
- * List all brands.
+ * Creates a new brand.
+ *
+ * @param data - The data for creating a new brand.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/create-a-brand}
  */
-export const queryBrandAPI = async (
-    params?: IGetQueryBrandParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = BRAND_PATH.QUERY;
-    const response = await jfwAxios.get<HttpResponseList<IBrand>>(url, {
-        ...config,
-        params,
-    });
-
-    return response.data;
-};
-
-/**
- * Gets brand by the given brand url.
- */
-export const getBrandByUrlAPI = async (config?: AxiosRequestConfig) => {
-    const url = BRAND_PATH.GET_BY_URL;
-
-    const response = await jfwAxios.get<HttpResponse<IBrand>>(url, config);
-
-    return response.data;
-};
-
-/**
- * Gets brand by the given brand id.
- */
-export const getBrandByIdAPI = async (
-    id: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(BRAND_PATH.GET_BY_ID, {
-        id,
-    });
-    const response = await jfwAxios.get<HttpResponse<IBrand>>(url, config);
-
-    return response.data;
-};
-
 export const createBrandAPI = async (
     data: ICreateBrandData,
     config?: AxiosRequestConfig,
 ) => {
-    const url = BRAND_PATH.CREATE;
-
+    const url = BRAND_PATH.CREATE_BRAND;
     const response = await jfwAxios.post<HttpResponse<IBrand>>(
         url,
         data,
@@ -69,41 +33,111 @@ export const createBrandAPI = async (
     return response.data;
 };
 
+/**
+ * Generates a new domain name.
+ *
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/generate-domain#get-api-v1-brands-generate-domain}
+ */
+export const generateNewDomainAPI = async (config?: AxiosRequestConfig) => {
+    const url = BRAND_PATH.GENERATE_NEW_DOMAIN;
+    const response = await jfwAxios.get<HttpResponse<IGeneratedDomain>>(
+        url,
+        config,
+    );
+
+    return response.data;
+};
+
+/**
+ * Gets brand by the given brand id.
+ *
+ * @param id - The id of the brand.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/get-a-brand}
+ */
+export const getBrandAPI = async (id: IdType, config?: AxiosRequestConfig) => {
+    const url = generatePath(BRAND_PATH.GET_BRAND, {
+        id,
+    });
+    const response = await jfwAxios.get<HttpResponse<IBrand>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * Get a brand based on the client domain.
+ *
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/get-a-brand-by-brand-url}
+ */
+export const getBrandByUrlAPI = async (config?: AxiosRequestConfig) => {
+    const url = BRAND_PATH.GET_BRAND_BY_URL;
+    const response = await jfwAxios.get<HttpResponse<IBrand>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * Get brands by the given filter.
+ *
+ * @param params - The parameters for getting brands.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/get-brands}
+ */
+export const getBrandsAPI = async (
+    params?: IGetBrandsParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = BRAND_PATH.GET_BRANDS;
+    const response = await jfwAxios.get<HttpResponseList<IBrand>>(url, {
+        ...config,
+        params,
+    });
+
+    return response.data;
+};
+
+/**
+ * Updates a brand by the given id.
+ *
+ * @param id - The id of the brand.
+ * @param data - The data for updating a brand.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/update-a-brand}
+ */
 export const updateBrandAPI = async (
     id: IdType,
     data: IUpdateBrandData,
     config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(BRAND_PATH.UPDATE_BY_ID, {
+    const url = generatePath(BRAND_PATH.UPDATE_BRAND, {
         id,
     });
-
-    // #NOT_SURE: HttpResponse<null>
-    const response = await jfwAxios.put<HttpResponse<null>>(url, data, config);
-
-    return response.data;
-};
-
-export const checkExistDomainUrlAPI = async (
-    domainUrl: string,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(BRAND_PATH.CHECK_DOMAIN_EXISTS, {
-        domainUrl,
-    });
-
-    const response = await jfwAxios.get<HttpResponse<boolean>>(url, config);
-
-    return response.data;
-};
-
-export const generateDomainAPI = async (config?: AxiosRequestConfig) => {
-    const url = BRAND_PATH.GENERATE_DOMAIN;
-
-    const response = await jfwAxios.get<HttpResponse<IGeneratedDomain>>(
+    const response = await jfwAxios.put<HttpResponse<boolean>>(
         url,
+        data,
         config,
     );
+
+    return response.data;
+};
+
+/**
+ * Checks if a brand exists in the database.
+ *
+ * @param domain The domain to check.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/brands/check-domain-exists}
+ */
+export const checkExistDomainUrlAPI = async (
+    domain: string,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(BRAND_PATH.CHECK_DOMAIN_IF_EXISTS, {
+        domain,
+    });
+    const response = await jfwAxios.get<HttpResponse<boolean>>(url, config);
 
     return response.data;
 };
--- src/models/brand/paths.ts
+++ src/models/brand/paths.ts
@@ -1,16 +1,13 @@
 const REST = 'brands';
 
 export const BRAND_PATH = {
-    QUERY: `v1/${REST}`,
-
-    GET_BY_ID: `${REST}/:id`,
-    GET_BY_URL: `v1/${REST}/by-url`,
-
-    CREATE: `v1/${REST}`,
-    UPDATE_BY_ID: `v1/${REST}/:id`,
-
-    CHECK_DOMAIN_EXISTS: `v1/${REST}/check-domain-exists/:domainUrl`,
-    GENERATE_DOMAIN: `v1/${REST}/generate-domain`,
+    CREATE_BRAND: `v1/${REST}`,
+    GET_BRAND: `v1/${REST}/:id`,
+    GET_BRAND_BY_URL: `v1/${REST}/by-url`,
+    GET_BRANDS: `v1/${REST}`,
+    UPDATE_BRAND: `v1/${REST}/:id`,
+    GENERATE_NEW_DOMAIN: `v1/${REST}/generate-domain`,
+    CHECK_DOMAIN_IF_EXISTS: `v1/${REST}/check-domain-exists/:domain`,
 };
 
 export { REST as REST_BRAND };
