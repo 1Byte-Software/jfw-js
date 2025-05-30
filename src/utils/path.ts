fe3b1d0e3b06da9553a4193bbf94c0d4c917e8f7
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-30 08:04:40 +0700

    feature: improve generatePath function support {} pattern

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,13 +1,16 @@
 // Generate path
 // Example: "Hello :friendName I'm :myName" with object {friendName: "Alice", myName: "Bob"} to "Hello Alice I'm Bob"
-export function generatePath(
-  str: string,
-  obj: { [key: string]: string | number },
+export function generatePath<T>(
+    str: string,
+    obj: T & { [K in keyof T]: string | number },
 ) {
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      str = str.replace(`:${key}`, obj[key]?.toString());
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            const value = obj[key]?.toString();
+            const colonPattern = new RegExp(`:${key}\\b`, 'g'); // Example: ":id"
+            const bracePattern = new RegExp(`{${key}}`, 'g'); // Example: "{id}"
+            str = str.replace(colonPattern, value).replace(bracePattern, value);
+        }
     }
-  }
-  return str;
+    return str;
 }
