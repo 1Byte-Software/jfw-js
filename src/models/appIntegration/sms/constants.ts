4150e60abd0839826051f1fc90eaeab3653e882d
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-03 15:45:05 +0700

    JFW-439 feature: improve app integration sms api

--- src/models/appIntegration/sms/constants.ts
+++ src/models/appIntegration/sms/constants.ts
@@ -1,3 +1,9 @@
+/**
+ * @remarks enum
+ * Possible values:
+ * - 0 - Inactive
+ * - 1 - Active
+ */
 export enum AppIntegrationSMSStatus {
     Active = 'Active',
     Inactive = 'Inactive',
--- src/models/appIntegration/sms/types.ts
+++ src/models/appIntegration/sms/types.ts
@@ -2,58 +2,48 @@ import { DateType, IdType } from '../../base';
 import { IAppIntegration } from '../types';
 import { AppIntegrationSMSStatus } from './constants';
 
-export interface IAppIntegrationSMS {
-    id: IdType;
-    appIntegration: IAppIntegration;
-    name: string;
-    username: string;
-    password: string;
-    phoneNumber: string;
-    description: string | null;
-    status: AppIntegrationSMSStatus;
-    isDefault: boolean;
-    createdDate: DateType;
-}
-
-//#region API types
-export interface IGetAppIntegrationSMSParams {}
-
 /**
- * Provides a data transfer object for the SMS integration which is used for command requests.
+ * Provides a data transfer object for the SMS integration.
  */
-export interface ICreateAppIntegrationSMSData {
+export interface IAppIntegrationSMS {
     /**
-     * The app integration provider ID of the SMS.
-     *
-     * @remarks string - min: 1
+     * The id of the object.
+     * @remarks min: 1
      */
-    appIntegrationId: string;
+    id: IdType;
+
+    /**
+     * The created date of the object.
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The app integration dto.
+     */
+    appIntegration: IAppIntegration;
 
     /**
      * The name of the SMS integration.
-     *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     name: string;
 
     /**
      * The username or identifier for the SMS integration. For Twilio, this is the Account SID (String Identifier).
-     *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     username: string;
 
     /**
      * The password or authentication token for the SMS integration. For Twilio, this is the Auth Token.
-     *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     password: string;
 
     /**
      * The phone number of the SMS integration. The phone number should be following the E.164 format.
-     *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     phoneNumber: string;
 
@@ -63,14 +53,70 @@ export interface ICreateAppIntegrationSMSData {
     description?: string | null;
 
     /**
-     * Possible values
-     * - `0` - `Inactive`
-     * - `1` - `Active`
+     * @remarks enum
+     */
+    status: AppIntegrationSMSStatus;
+
+    /**
+     * Flag to indicate if the SMS integration is the default.
+     */
+    isDefault: boolean;
+}
+
+//#region API types
+/**
+ * Provides a data transfer object for the SMS integration which is used for command requests.
+ */
+export interface ICreateAppIntegrationSMSParams {
+    /**
+     * The app integration provider ID of the SMS.
+     *
+     * @remarks min: 1
+     */
+    appIntegrationId: string;
+
+    /**
+     * The name of the SMS integration.
+     *
+     * @remarks min: 1
+     */
+    name: string;
+
+    /**
+     * The username or identifier for the SMS integration. For Twilio, this is the Account SID (String Identifier).
+     *
+     * @remarks min: 1
+     */
+    username: string;
+
+    /**
+     * The password or authentication token for the SMS integration. For Twilio, this is the Auth Token.
+     *
+     * @remarks min: 1
+     */
+    password: string;
+
+    /**
+     * The phone number of the SMS integration. The phone number should be following the E.164 format.
+     *
+     * @remarks min: 1
+     */
+    phoneNumber: string;
+
+    /**
+     * The description of the SMS integration.
+     */
+    description?: string | null;
+
+    /**
+     * The status of the app integration.
+     *
+     * @remarks enum
      */
     status?: AppIntegrationSMSStatus;
 
     /**
-     * Flag to indicate if the app integration sms setting is default.
+     * Flag to indicate if the app integration SMS setting is default.
      *
      * @defaultValue `false`
      */
@@ -80,46 +126,99 @@ export interface ICreateAppIntegrationSMSData {
 /**
  * Provides a data transfer object for the SMS integration which is used for command requests.
  */
-export type IUpdateAppIntegrationSMSData = ICreateAppIntegrationSMSData;
+export interface IUpdateAppIntegrationSMSParams {
+    /**
+     * The app integration provider ID of the SMS.
+     *
+     * @remarks min: 1
+     */
+    appIntegrationId: string;
 
-/**
- * Represents the data required to test sending an SMS using an external SMS integration (e.g., Twilio). This DTO is typically used to verify the integration credentials and messaging capabilities.
- */
-export interface ITestSendingSMSMessageData {
+    /**
+     * The name of the SMS integration.
+     *
+     * @remarks min: 1
+     */
+    name: string;
+
     /**
      * The username or identifier for the SMS integration. For Twilio, this is the Account SID (String Identifier).
      *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     username: string;
 
     /**
      * The password or authentication token for the SMS integration. For Twilio, this is the Auth Token.
      *
-     * @remarks string - min: 1
+     * @remarks min: 1
+     */
+    password: string;
+
+    /**
+     * The phone number of the SMS integration. The phone number should be following the E.164 format.
+     *
+     * @remarks min: 1
+     */
+    phoneNumber: string;
+
+    /**
+     * The description of the SMS integration.
+     */
+    description?: string | null;
+
+    /**
+     * The status of the app integration.
+     *
+     * @remarks enum
+     */
+    status?: AppIntegrationSMSStatus;
+
+    /**
+     * Flag to indicate if the app integration SMS setting is default.
+     *
+     * @defaultValue `false`
+     */
+    isDefault?: boolean;
+}
+
+/**
+ * Represents the data required to test sending an SMS using an external SMS integration (e.g., Twilio). This DTO is typically used to verify the integration credentials and messaging capabilities.
+ */
+export interface ITestSendingSMSMessageParams {
+    /**
+     * The username or identifier for the SMS integration. For Twilio, this is the Account SID (String Identifier).
+     *
+     * @remarks min: 1
+     */
+    username: string;
+
+    /**
+     * The password or authentication token for the SMS integration. For Twilio, this is the Auth Token.
+     *
+     * @remarks min: 1
      */
     password: string;
 
     /**
      * The sender's phone number used by the SMS integration. Must be in E.164 format (e.g., +1234567890).
      *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     phoneNumberFrom: string;
 
     /**
      * The recipient's phone number to which the test SMS will be sent. Must be in E.164 format (e.g., +1234567890).
      *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     phoneNumberTo: string;
 
     /**
      * The content of the SMS message to be sent during the test.
      *
-     * @remarks string - min: 1
+     * @remarks min: 1
      */
     message: string;
 }
-
 //#endregion
