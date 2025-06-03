00358db8976d7e249165c0c92cabdf277e66057d
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-03 09:13:02 +0700

    JFW-491 refactor: Refactor JFWError

--- src/core/error/index.ts
+++ src/core/error/index.ts
@@ -1,2 +1,2 @@
-export * from './error';
+export * from './constants';
 export * from './types';
--- src/core/error/types.ts
+++ src/core/error/types.ts
@@ -1,13 +1,33 @@
 import { AxiosError } from 'axios';
-import { JFWLogLevel } from './constants';
 import { HttpResponse } from '../query';
+import { JFWLogLevel } from './constants';
 
 export type JFWError = {
-    code: number;
-    subcode: number;
-    errorCode: number;
-    details: string;
-    logLevel: JFWLogLevel;
+    /**
+     * @remarks int32
+     */
+    code?: number;
+
+    /**
+     * @remarks int32
+     */
+    subcode?: number;
+
+    /**
+     * @remarks int32
+     */
+    errorCode?: number;
+
+    /**
+     * @remarks int32
+     * @readonly
+     */
+    details?: string | null;
+
+    /**
+     * @remarks enum
+     */
+    logLevel?: JFWLogLevel | null;
 };
 
 export type THttpError = Error | AxiosError<HttpResponse>;
