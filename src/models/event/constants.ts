8373165cfd00acd4931b2031b1d29ef925544fce
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-04 16:16:55 +0700

    JFW-451 feature: improve event api

--- src/models/event/constants.ts
+++ src/models/event/constants.ts
@@ -1,4 +1,9 @@
+/**
+ * Possible values:
+ * - `0` - `Inactive`
+ * - `1` - `Active`
+ */
 export enum EventStatus {
-    Active = 'Active',
     Inactive = 'Inactive',
+    Active = 'Active',
 }
--- src/models/event/types.ts
+++ src/models/event/types.ts
@@ -1,42 +1,243 @@
 import { IPageable, ISortable } from '../../core';
-import { IBaseObject, IdType } from '../base';
+import { DateType, IBaseObject, IdType } from '../base';
 import { ILanguage } from '../language';
 import { EventStatus } from './constants';
 
-export interface IEvent extends IBaseObject {
+/**
+ * This class represents the data transfer object for the email template.
+ */
+export interface IEmailTemplate {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     *
+     * @example "Kd12AnplKdMd5s"
+     */
+    id: string;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The country code of the email.
+     * This value is used to determine the country of the email.
+     * Following the ISO 3166-1 alpha-2 standard. For example, "US" for United States, "FR" for France, etc.
+     */
+    countryCode?: string | null;
+
+    /**
+     * The language code of the email.
+     * This value is used to determine the language of the email.
+     * Following the ISO 639-1 standard. For example, "en" for English, "fr" for French, etc.
+     *
+     * @remarks min: 1
+     */
+    languageCode: string;
+
+    /**
+     * The subject of the email.
+     *
+     * @remarks min: 1
+     */
+    subject: string;
+
+    /**
+     * The body of the email.
+     *
+     * @remarks min: 1
+     */
+    body: string;
+
+    /**
+     * The tags of the email.
+     */
+    tags?: string | null;
+
+    event: IEvent;
+}
+
+/**
+ * This class represents the EventDto class.
+ */
+export interface IEvent {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
 
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The guid of the event.
+     *
+     * @remarks uuid
+     */
     guid: string;
+
+    /**
+     * The group name of the event.
+     *
+     * @remarks min: 1
+     */
     groupCodeName: string;
+
+    /**
+     * The code of the event.
+     *
+     * @remarks min: 1
+     */
     code: string;
 
+    /**
+     * The name of the event.
+     *
+     * @remarks min: 1
+     */
     name: string;
-    description: string;
+
+    /**
+     * The description of the tracking event.
+     */
+    description?: string | null;
+
+    /**
+     * The tracking level of the event.
+     *
+     * @remarks int32
+     */
     trackingLevel: number;
-    tags: string;
-    zOrder: number;
+
+    /**
+     * The tags of the event.
+     */
+    tags?: string | null;
+
+    /**
+     * The z-order of the event.
+     *
+     * @remarks int64
+     */
+    zOrder?: number | null;
+
+    /**
+     * The status of the event.
+     *
+     * @remarks enum
+     */
     status: EventStatus;
 
+    /**
+     * Flag indicating if the event is a system event.
+     */
     isSystem: boolean;
 }
 
+/**
+ * This class is used to represent the wallet earning event DTO.
+ */
 export interface IWalletEarningEvent {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
-    trackingEventId: IdType;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The id of the event.
+     *
+     * @remarks min: 1
+     */
+    eventId: IdType;
+
+    /**
+     * The code of the wallet earning event.
+     *
+     * @remarks min: 1
+     */
     code: string;
+
+    /**
+     * The name of the wallet earning event.
+     *
+     * @remarks min: 1
+     */
     name: string;
+
+    /**
+     * The amount of the wallet earning event.
+     *
+     * @remarks double
+     */
     amount: number;
-    description: string;
+
+    /**
+     * The description of the wallet earning event.
+     */
+    description?: string | null;
 }
 
+/**
+ * This class represents the data transfer object for the email template default.
+ */
 export interface IDefaultEmailTemplate {
-    languageCode: ILanguage['code'];
+    /**
+     * The language code of the email.
+     * This value is used to determine the language of the email.
+     * Following the ISO 639-1 standard. For example, "en" for English, "fr" for French, etc.
+     *
+     * @remarks min: 1
+     */
+    languageCode: string;
+
+    /**
+     * The subject of the email.
+     *
+     * @remarks min: 1
+     */
     subject: string;
+
+    /**
+     * The body of the email.
+     *
+     * @remarks min: 1
+     */
     body: string;
 }
 
 export interface IDefaultPhoneTemplate {
-    languageCode: ILanguage['code'];
+    /**
+     * The language code of the email. This value is used to determine the language of the email.
+     * Following the ISO 639-1 standard. For example, "en" for English, "fr" for French, etc.
+     *
+     * @remarks min: 1
+     */
+    languageCode: string;
+
+    /**
+     * The content of the phone template.
+     *
+     * @remarks min: 1
+     */
     content: string;
 }
 
@@ -75,6 +276,8 @@ export interface IGetEventsParams extends IPageable, ISortable {
 
     /**
      * Filter by Status.
+     *
+     * @remarks enum
      */
     status?: EventStatus;
 
@@ -102,9 +305,18 @@ export interface IGetEventsEarningWalletParams {
     type?: string;
 }
 
-export interface IGetDefaultEmailTemplateParams {
-    languageCode?: string;
+export interface IGetEmailTemplateDefaultByEventParams {
+    /**
+     * The code of the language. The language code must be in the ISO 639-1 format.
+     */
+    languageCode: string;
 }
-export type IGetDefaultPhoneTemplateParams = IGetDefaultEmailTemplateParams;
+
+export interface IGetPhoneTemplateDefaultByEventParams {
+    /**
+     * The code of the language. The language code must be in the ISO 639-1 format.
+     */
+    languageCode: string;
+}
 
 //#endregion
