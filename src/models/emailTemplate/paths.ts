8d3456c8debca9fcbeff81b54da2563c1267fc49
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-04 16:47:10 +0700

    JFW-453 feature: improve email template api

--- src/models/emailTemplate/paths.ts
+++ src/models/emailTemplate/paths.ts
@@ -3,12 +3,12 @@ const REST_EVENTS = 'events';
 
 export const EMAIL_TEMPLATE_PATH = {
     CREATE_EMAIL_TEMPLATE: `v1/${REST}`,
-    DELETE_EMAIL_TEMPLATE: `v1/${REST}/:id`,
+    DELETE_EMAIL_TEMPLATE: `v1/${REST}/{id}`,
 
-    GET_EMAIL_TEMPLATE_BY_EVENT: `v1/${REST_EVENTS}/:eventId/${REST}`,
-    GET_EMAIL_TEMPLATE: `v1/${REST}/:id`,
+    GET_EMAIL_TEMPLATE: `v1/${REST}/{id}`,
     GET_EMAIL_TEMPLATES: `v1/${REST}`,
     GET_ALL_PLACEHOLDER_KEYS_AVAILABLE: `v1/${REST}/placeholder-keys`,
+    GET_EMAIL_TEMPLATE_BY_EVENT: `v1/${REST_EVENTS}/{eventId}/${REST}`,
 
-    UPDATE_EMAIL_TEMPLATE: `v1/${REST}/:id`,
+    UPDATE_EMAIL_TEMPLATE: `v1/${REST}/{id}`,
 };
