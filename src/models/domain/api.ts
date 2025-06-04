e7c9ffd912a61fbcac69b0fc0ec6542b3c907f7d
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-04 15:11:39 +0700

    JFW-450 feature: improve domain and brandlinks api

--- src/models/domain/api.ts
+++ src/models/domain/api.ts
@@ -1,19 +1,45 @@
 import { AxiosRequestConfig } from 'axios';
-import {
-    IDomain,
-    ICreateDomainData,
-    IGetDomainsParams,
-    IUpdateDomainData,
-} from './types';
-import { DOMAIN_PATH } from './paths';
-import { jfwAxios } from '../../core/client/client';
 import { HttpResponse, HttpResponseList } from '../../core';
+import { jfwAxios } from '../../core/client/client';
 import { generatePath } from '../../utils/path';
 import { IdType } from '../base';
+import { DOMAIN_PATH } from './paths';
+import {
+    ICreateDomainParams,
+    IDomain,
+    IGetDomainsParams,
+    IUpdateDomainParams,
+} from './types';
 
 /**
- * Waiting for backend documentation, will update later
- * #JFW-359
+ * # Delete a Coupon
+ *
+ * Delete a domain
+ *
+ * @param id - The id of the domain
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/domains/delete-a-domain}
+ */
+export const deleteDomainAPI = async (
+    id: IdType,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(DOMAIN_PATH.DELETE_DOMAIN, {
+        id,
+    });
+    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * # Get domains
+ *
+ * Get domains
+ *
+ * @param params - The params for getting domains.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/domains/get-domains}
  */
 export const getDomainsAPI = async (
     params?: IGetDomainsParams,
@@ -29,10 +55,34 @@ export const getDomainsAPI = async (
 };
 
 /**
- * #NOTE: This API will update in the feature.
+ * Get a domain
+ *
+ * Get a domain
+ *
+ * @param id - The id of the domain.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/domains/get-a-domain}
+ */
+export const getCoupon = async (id: IdType, config?: AxiosRequestConfig) => {
+    const url = generatePath(DOMAIN_PATH.GET_DOMAIN, {
+        id,
+    });
+    const response = await jfwAxios.get<HttpResponse<IDomain>>(url, config);
+
+    return response.data;
+};
+
+/**
+ * # Create a Domain
+ *
+ * Creates a new domain.
+ *
+ * @param params - The params for creating a domain.
+ * @param config - Optional axios request configuration object.
+ * @see {@link }
  */
 export const createDomainAPI = async (
-    data: ICreateDomainData,
+    data: ICreateDomainParams,
     config?: AxiosRequestConfig,
 ) => {
     const url = DOMAIN_PATH.CREATE_DOMAIN;
@@ -46,31 +96,23 @@ export const createDomainAPI = async (
 };
 
 /**
- * #NOTE: This API will update in the feature.
+ * # Update a Domain
+ *
+ * Updates a domain by the given id.
+ *
+ * @param id - The id of the domain.
+ * @param params - The params for updating a domain.
+ * @param config - Optional axios request configuration object.
+ * @see {@link }
  */
 export const updateDomainAPI = async (
     id: IdType,
-    data: IUpdateDomainData,
+    data: IUpdateDomainParams,
 ) => {
     const url = generatePath(DOMAIN_PATH.UPDATE_DOMAIN, {
         id,
     });
     const response = await jfwAxios.put<HttpResponse<boolean>>(url, data);
-
-    return response.data;
-};
-
-/**
- * #NOTE: This API will update in the feature.
- */
-export const deleteDomainAPI = async (
-    id: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(DOMAIN_PATH.DELETE_DOMAIN, {
-        id,
-    });
-    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
 
     return response.data;
 };
