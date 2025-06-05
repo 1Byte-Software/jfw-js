f256afc1565ae0b6d388959a993ddbc3d9d23aba
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-05 16:31:47 +0700

    JFW-464 feature: improve payment api

--- src/models/payment/constants.ts
+++ src/models/payment/constants.ts
@@ -1,68 +1,17 @@
 /**
- * Define an object to represent different payment status.
- *
- * @deprecated use PaymentStatus enum instead.
+ * Possible values:
+ * - `0` - `Waiting`
+ * - `1` - `Completed`
+ * - `2` - `Refunded`
+ * - `3` - `Failed`
+ * - `4` - `Pending`
+ * - `5` - `Denied`
+ * - `6` - `Reserved`
+ * - `7` - `CanceledReversal`
+ * - `-3` - `Deleted`
+ * - `-2` - `Voided`
+ * - `-1` - `Expired`
  */
-export const PAYMENT_STATUS = {
-    voided: 'Voided',
-    expired: 'Expired',
-    waiting: 'Waiting',
-    completed: 'Completed',
-    refunded: 'Refunded',
-    failed: 'Failed',
-    pending: 'Pending',
-    denied: 'Denied',
-    reserved: 'Reserved',
-    canceledReversal: 'Canceled Reversal',
-};
-
-/**
- * Define an array of payment status options for selection in a dropdown or similar UI component
- * @deprecated Will delete in feature. This code should not be defined in jfw-js, this is front end business.
- */
-export const PAYMENT_STATUS_OPTIONS = [
-    {
-        label: 'Voided',
-        value: 'Voided',
-    },
-    {
-        label: 'Expired',
-        value: 'Expired',
-    },
-    {
-        label: 'Waiting',
-        value: 'Waiting',
-    },
-    {
-        label: 'Completed',
-        value: 'Completed',
-    },
-    {
-        label: 'Refunded',
-        value: 'Refunded',
-    },
-    {
-        label: 'Failed',
-        value: 'Failed',
-    },
-    {
-        label: 'Pending',
-        value: 'Pending',
-    },
-    {
-        label: 'Denied',
-        value: 'Denied',
-    },
-    {
-        label: 'Reserved',
-        value: 'Reserved',
-    },
-    {
-        label: 'Canceled Reversal',
-        value: 'Canceled Reversal',
-    },
-];
-
 export enum PaymentStatus {
     Waiting = 'Waiting',
     Completed = 'Completed',
--- src/models/payment/paths.ts
+++ src/models/payment/paths.ts
@@ -1,8 +1,7 @@
 const REST = 'payments';
 
 export const PAYMENT_PATH = {
-    GET_PAYMENT: `v1/${REST}/:id`,
+    GET_PAYMENT: `v1/${REST}/{id}`,
     GET_PAYMENTS: `v1/${REST}`,
-
     GET_OVERVIEW_SELL_DASHBOARD: `v1/${REST}/dashboard`,
 };
