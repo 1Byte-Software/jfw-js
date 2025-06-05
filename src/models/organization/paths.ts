245e4d59764f70c70d0fde8ce0fc9890fc2b8023
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-05 14:59:19 +0700

    JFW-461 feature: improve organization api

--- src/models/organization/paths.ts
+++ src/models/organization/paths.ts
@@ -2,21 +2,17 @@ const REST = 'organizations';
 const REST_USER = 'users';
 
 export const ORGANIZATION_PATH = {
-    GET: `v1/${REST}`,
-    CREATE: `v1/${REST}`,
+    ADD_NEW_USER_TO_ORGANIZATION: `v1/${REST}/{id}/${REST_USER}/{userId}`,
+    ADD_USER_FOR_DEFAULT_LANGUAGE_CODE_ORGANIZATION_SYSTEM: `v1/${REST}/${REST_USER}/{userId}`,
+    ASSIGN_ROLE_FOR_USER_IN_ORGANIZATION: `v1/${REST}/{id}/assign-role-users`,
 
-    GET_BY_ID: `v1/${REST}/:id`,
-    UPDATE_BY_ID: `v1/${REST}/:id`,
-    DELETE_BY_ID: `v1/${REST}/:id`,
+    CREATE_ORGANIZATION: `v1/${REST}`,
+    DELETE_ORGANIZATION: `v1/${REST}/{id}`,
+    GET_ORGANIZATION: `v1/${REST}/{id}`,
+    GET_ORGANIZATIONS: `v1/${REST}`,
 
-    ASSIGN_ROLE_USERS: `v1/${REST}/:id/assign-role-users`,
-
-    USERS: {
-        GET: `v1/${REST}/:id/${REST_USER}`,
-        ADD_TO_ORGANIZATION: `v1/${REST}/:id/${REST_USER}/:userId`,
-        ADD_FOR_DEFAULT_LANGUAGE_CODE_ORGANIZATION_SYSTEM: `v1/${REST}/${REST_USER}/:userId`,
-
-        UPDATE_STATUS: `v1/${REST}/:id/${REST_USER}`,
-        REMOVE_FROM_ORGANIZATION: `v1/${REST}/:id/${REST_USER}/:userId`,
-    },
+    GET_USERS_OF_ORGANIZATION: `v1/${REST}/{id}/${REST_USER}`,
+    REMOVE_USERS_IN_ORGANIZATION: `v1/${REST}/{id}/${REST_USER}/{userId}`,
+    UPDATE_ORGANIZATION: `v1/${REST}/{id}`,
+    UPDATE_USER_STATUS_IN_ORGANIZATION: `v1/${REST}/{id}/${REST_USER}`,
 };
--- src/models/organization/types.ts
+++ src/models/organization/types.ts
@@ -1,103 +1,309 @@
-import { DateType, IdType } from '../base';
 import { IPageable, ISortable } from '../../core';
-import { IMedia } from '../cdn';
-import { ILanguage } from '../language';
-import { IUser } from '../user';
-import { OrganizationUserStatus } from './constants';
+import { DateType, IdType } from '../base';
+import { IBaseUser } from '../user';
+import { OrganizationStatus, OrganizationUserStatus } from './constants';
 
+/**
+ * The Organization Data Transfer Object.
+ */
 export interface IOrganization {
-    avatar?: string;
-    name: string;
-    totalMembers: number;
-    countryCode: string;
-    createdBy: string;
-    createdDate: DateType;
-    description?: string;
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
-    languageCode: string;
-    medias?: IMedia[];
-    modifiedDate: DateType;
-    isPrivate: boolean;
-    status: string;
-    statusUser?: string;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The code of the organization
+     *
+     * @remarks min: 1
+     */
     code: string;
+
+    /**
+     * The name of the organization
+     *
+     * @remarks min: 1
+     */
+    name: string;
+
+    /**
+     * The Organization Language Code.
+     */
+    languageCode?: string | null;
+
+    /**
+     * The Organization Country Code.
+     */
+    countryCode?: string | null;
+
+    /**
+     * The Organization Avatar.
+     *
+     * @remarks uri
+     */
+    avatar?: string | null;
+
+    /**
+     * The Organization Is Default.
+     *
+     * @defaultValue `false`
+     */
+    isDefault?: boolean | null;
+
+    /**
+     * The Organization Is Private.
+     */
+    isPrivate: boolean;
+
+    /**
+     * The Organization Is System.
+     */
     isSystem: boolean;
-    isDefault: boolean;
-    language?: ILanguage;
+
+    /**
+     * The Organization Description.
+     */
+    description?: string | null;
+
+    /**
+     * The status of the organization.
+     *
+     * @remarks enum
+     */
+    status: OrganizationStatus;
+
+    /**
+     * The user status in the organization.
+     *
+     * @remarks enum
+     */
+    statusUser?: OrganizationUserStatus;
+
+    /**
+     * The Total Members in the Organization.
+     *
+     * @remarks int32
+     */
+    totalMembers: number;
+
+    /**
+     * The Organization Created By.
+     */
+    createdBy?: string | null;
 }
 
+/**
+ * This class represents the Organization User Data Transfer Object.
+ */
 export interface IOrganizationUser {
+    /**
+     * This class is used to return user information to client.
+     */
+    user: IBaseUser;
+
+    /**
+     * The role code.
+     */
+    roleCode?: string | null;
+
+    /**
+     * The user status in the organization.
+     *
+     * @remarks enum
+     */
+    statusUser: OrganizationUserStatus;
+
+    /**
+     * The date the user joined the organization.
+     *
+     * @remarks date-time
+     */
     joinedDate: DateType;
-    statusUser: string;
-    user: IUser;
-    roleCode: string;
 }
 
 //#region API types
-export interface IGetOrganizationParams extends IPageable {
-    keywords?: string;
-    userId?: string;
-    languageCode?: string;
-    name?: string;
-    pageSize?: number;
-    pageNumber?: number;
-    brandCode?: string;
-    code?: string;
-    status?: string;
-    isPrivate?: boolean;
-    viewerCode?: string;
-}
-export interface IGetOrganizationDetailPath {
-    identify: string;
-}
+export interface IGetOrganizationsParams extends IPageable, ISortable {
+    /**
+     * The param UserId to apply filter.
+     */
+    userId?: IdType;
 
-export interface IUpdateUserStatusInOrganizationParams {
-    organizationId: IdType;
-}
-
-export interface IUpdateUserStatusInOrganizationData {
-    userIds: IdType[];
-    status: OrganizationUserStatus;
-}
-
-export interface IRemoveUserInOrganizationParams {
-    organizationId: IdType;
-    userId: IdType;
-}
-export interface IAddNewUserToOrganizationParams {
-    organizationId: IdType;
-    userId: IdType;
-}
-
-export interface IAddUserForDefaultLanguageCodeOrganizationSystemParams {
-    userId: IdType;
-    languageCode: string;
-}
-
-export interface IAssignRoleForUserInOrganizationParams {
-    organizationId: IdType;
-}
-
-export interface IAssignRoleForUserInOrganizationData {
-    roleId: IdType;
-    userId: IdType;
-}
-
-export interface IGetUsersOfOrganizationParams extends IPageable, ISortable {
-    organizationId: IdType;
-
-    status?: string;
-}
-export interface ICreateOrganizationData {
+    /**
+     * The param CountryCode to apply filter.
+     */
     countryCode?: string;
+
+    /**
+     * The param LanguageCode to apply filter.
+     */
     languageCode?: string;
-    name: string;
+
+    /**
+     * The param Code to apply filter.
+     */
+    code?: string;
+
+    /**
+     * The param Name to apply filter.
+     */
+    name?: string;
+
+    /**
+     * The param Description to apply filter.
+     */
     description?: string;
+
+    /**
+     * The param Avatar to apply filter.
+     */
     avatar?: string;
+
+    /**
+     * The param IsPrivate to apply filter.
+     */
     isPrivate?: boolean;
-    isDefault?: boolean;
-    status?: string;
+
+    /**
+     * The param Status to apply filter.
+     *
+     * @remarks enum
+     */
+    status?: OrganizationStatus;
+
+    /**
+     * The param Status to apply filter. This status is the status of the user in the organization.
+     *
+     * @remarks enum
+     */
+    userStatus?: OrganizationUserStatus;
+
+    /**
+     * The param Keywords to apply filter.
+     */
+    keywords?: string;
 }
 
-export type IUpdateOrganizationData = ICreateOrganizationData;
+/**
+ * This class represents the Organization User Join Request Data Transfer Object.
+ */
+export interface IUpdateUserStatusInOrganizationParams {
+    /**
+     * @remarks enum
+     */
+    status: OrganizationUserStatus;
+
+    /**
+     * The list of user ids.
+     */
+    userIds: IdType[];
+}
+
+/**
+ * This class provides the assign user role organization dto.
+ */
+export interface IAssignRoleForUserInOrganizationParams {
+    /**
+     * The id of the role to assign.
+     *
+     * @remarks min: 1
+     */
+    roleId: IdType;
+
+    /**
+     * The id of the user to assign the role to.
+     *
+     * @remarks min: 1
+     */
+    userId: IdType;
+}
+
+/**
+ * Params for getting users of an Organization.
+ */
+export interface IGetUsersOfOrganizationParams extends IPageable, ISortable {
+    /**
+     * The user id filter.
+     */
+    userId?: IdType;
+
+    /**
+     * The role id filter.
+     */
+    roleId?: IdType;
+
+    /**
+     * Filter by status of the user in the organization.
+     *
+     * @remarks enum
+     */
+    status?: OrganizationUserStatus | null;
+}
+
+/**
+ * This class represents the request model for command requests related to organizations.
+ */
+export interface ICreateOrganizationParams {
+    /**
+     * The country code of the organization.
+     */
+    countryCode?: string | null;
+
+    /**
+     * The language code of the organization.
+     */
+    languageCode?: string | null;
+
+    /**
+     * The name of the organization.
+     *
+     * @remarks min: 1
+     */
+    name: string;
+
+    /**
+     * The description of the organization.
+     *
+     * @remarks min: 1
+     */
+    description: string;
+
+    /**
+     * The avatar of the organization.
+     *
+     * @remarks uri
+     */
+    avatar?: string | null;
+
+    /**
+     * The is private of the organization. By default, it is set to false.
+     *
+     * @defaultValue `false`
+     */
+    isPrivate?: boolean | null;
+
+    /**
+     * The is default of the organization. By default, it is set to false.
+     *
+     * @defaultValue `false`
+     */
+    isDefault?: boolean | null;
+
+    /**
+     * The status of the organization.
+     *
+     * @remarks enum
+     */
+    status?: OrganizationStatus | null;
+}
+
+export type IUpdateOrganizationParams = ICreateOrganizationParams;
 //#endregion
