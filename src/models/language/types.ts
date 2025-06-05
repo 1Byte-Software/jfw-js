7e45a6736f1ab71d51674d320a3ff852d3d82fdd
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-05 10:35:45 +0700

    JFW-458 feature: improve language api

--- src/models/language/types.ts
+++ src/models/language/types.ts
@@ -1,17 +1,59 @@
-import { IdType } from '../base';
+import { DateType, IdType } from '../base';
 
+/**
+ * This class represents the language data transfer object.
+ */
 export interface ILanguage {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
-    code: string;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType;
+
+    /**
+     * The country code.
+     *
+     * @remarks min: 1
+     */
     countryCode: string;
-    name: string;
-    nativeName: string;
+
+    /**
+     * The language code.
+     *
+     * @remarks min: 1
+     */
+    code: string;
+
+    /**
+     * The language name.
+     */
+    name?: string | null;
+
+    /**
+     * The native language name.
+     */
+    nativeName?: string | null;
+
+    /**
+     * The date format.
+     *
+     * @remarks min: 1
+     */
     dateFormat: string;
-    tags: string;
+
+    /**
+     * The language tags.
+     */
+    tags?: string | null;
 }
 
 //#region API types
-export interface IGetLanguagesParams {
-    
-}
 //#endregion
