0cca96bd92c3fbc34993f0d2f88cbb50cd7b4caf
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-05 21:53:25 +0700

    JFW-471 feature: improve tracking activity api

--- src/models/trackingActivity/api.ts
+++ src/models/trackingActivity/api.ts
@@ -7,8 +7,12 @@ import { TRACKING_ACTIVITY_PATH } from './paths';
 import { IGetTrackingActivitiesParams, ITrackingActivity } from './types';
 
 /**
- * Gets the activities by the specified filter.
+ * # Get activities
  *
+ * Get the list of tracking activities.
+ *
+ * @param params - The params for getting tracking activities.
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-activities/get-activities}
  */
 export const getTrackingActivitiesAPI = async (
@@ -16,7 +20,6 @@ export const getTrackingActivitiesAPI = async (
     config?: AxiosRequestConfig,
 ) => {
     const url = TRACKING_ACTIVITY_PATH.GET_ACTIVITIES;
-
     const response = await jfwAxios.get<HttpResponseList<ITrackingActivity>>(
         url,
         {
@@ -29,8 +32,12 @@ export const getTrackingActivitiesAPI = async (
 };
 
 /**
+ * # Get a activity
+ *
  * Get a tracking activity by the given id.
  *
+ * @param id - The id of the activity.
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-activities/get-a-activity}
  */
 export const getTrackingActivityAPI = async (
@@ -40,7 +47,6 @@ export const getTrackingActivityAPI = async (
     const url = generatePath(TRACKING_ACTIVITY_PATH.GET_ACTIVITY, {
         id,
     });
-
     const response = await jfwAxios.get<HttpResponse<ITrackingActivity>>(
         url,
         config,
--- src/models/trackingActivity/types.ts
+++ src/models/trackingActivity/types.ts
@@ -3,74 +3,194 @@ import { DateType, IdType } from '../base';
 import { IEvent } from '../event';
 import { IBaseUser } from '../user';
 
+/**
+ * This class represents the data transfer object for the tracking activity.
+ */
 export interface ITrackingActivity {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
-    userId: IdType;
-    createdDate: DateType;
-    description: string;
-    deviceId: IdType;
-    event: IEvent;
-    longitude: number;
-    latitude: number;
-    location: string;
-    browserName: string;
-    os: string;
-    ipAddress: string;
-    URL: string;
-    eventId: IdType;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * This class is used to return user information to client.
+     */
     user: IBaseUser;
+
+    /**
+     * The device id.
+     */
+    deviceId?: IdType | null;
+
+    /**
+     * This class represents the EventDto class.
+     */
+    event: IEvent;
+
+    /**
+     * The url requested.
+     *
+     * @remarks uri
+     */
+    url?: string | null;
+
+    /**
+     * The ip address of the user.
+     *
+     * @remarks min: 1
+     */
+    ipAddress: string;
+
+    /**
+     * The operating system of the user.
+     */
+    os?: string | null;
+
+    /**
+     * The browser name.
+     */
+    browserName?: string | null;
+
+    /**
+     * The browser version.
+     */
+    browserVersion?: string | null;
+
+    /**
+     * Flag to indicate if the request is from a mobile device.
+     */
+    requestFromMobile?: boolean | null;
+
+    /**
+     * The user agent.
+     *
+     * @remarks min: 1
+     */
+    userAgent: string;
+
+    /**
+     * The location of the user.
+     */
+    location?: string | null;
+
+    /**
+     * The latitude of the user request.
+     *
+     * @remarks double
+     */
+    latitude?: number | null;
+
+    /**
+     * The longitude of the user request.
+     *
+     * @remarks double
+     */
+    longitude?: number | null;
+
+    /**
+     * The description of the tracking activity.
+     */
+    description?: string | null;
+
+    /**
+     * The tags associated with the tracking activity.
+     */
+    tags?: string | null;
 }
+
 //#region API types
 export interface IGetTrackingActivitiesParams extends IPageable, ISortable {
-    /** Filter by user id */
-    userId?: IdType;
-    /** Filter by device id */
-    deviceId?: null;
-    /** Filter by tracking event id */
-    eventId?: IdType;
-    /** Filter by the url requested */
-    URL?: string;
-    /** Filter by the ip address */
+    /**
+     * Filter by user id.
+     */
+    userId?: string;
+
+    /**
+     * Filter by device id.
+     */
+    deviceId?: string;
+
+    /**
+     * Filter by tracking event id.
+     */
+    trackingEventId?: string;
+
+    /**
+     * Filter by the URL requested.
+     */
+    url?: string;
+
+    /**
+     * Filter by the ip address.
+     */
     ipAddress?: string;
-    /** Filter by the os */
+
+    /**
+     * Filter by the os.
+     */
     os?: string;
-    /** Filter by the browser name */
+
+    /**
+     * Filter by the browser name.
+     */
     browserName?: string;
-    /** Filter by the browser version */
+
+    /**
+     * Filter by the browser version.
+     */
     browserVersion?: string;
-    /** Filter by the request from mobile */
+
+    /**
+     * Filter by the request from mobile.
+     */
     requestFromMobile?: boolean;
-    /** Filter by the user agent */
+
+    /**
+     * Filter by the user agent.
+     */
     userAgent?: string;
-    /** Filter by the location */
+
+    /**
+     * Filter by the location.
+     */
     location?: string;
-    /** Filter by the latitude */
+
+    /**
+     * Filter by the latitude.
+     *
+     * @remarks double
+     */
     latitude?: number;
-    /** Filter by the longitude */
+
+    /**
+     * Filter by the longitude.
+     *
+     * @remarks double
+     */
     longitude?: number;
-    /** Filter by the description */
+
+    /**
+     * Filter by the description.
+     */
     description?: string;
-    /** Filter by the tags */
+
+    /**
+     * Filter by the tags.
+     */
     tags?: string;
-    /** Filter by the keywords */
+
+    /**
+     * Filter by the keywords.
+     */
     keywords?: string;
 }
-
-export interface ICreateTrackingActivityParams {
-    browserName: string;
-    browserVersion: string;
-    description: string;
-    deviceId: null;
-    ipAddress: string;
-    latitude: number;
-    location: string;
-    longitude: number;
-    os: string;
-    requestFromMobile: boolean;
-    eventId: IdType;
-    URL: string;
-    userAgent: string;
-    userId: IdType;
-}
-
 //#endregion
