a5151af2b401a171dfaa050b9efd082830c15ed1
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-06 14:49:55 +0700

    JFW-505 feature: remove API suffix

--- src/models/timezone/api.ts
+++ src/models/timezone/api.ts
@@ -2,22 +2,19 @@ import { AxiosRequestConfig } from 'axios';
 import { HttpResponse } from '../../core';
 import { jfwAxios } from '../../core/client/client';
 import { TIMEZONE_PATH } from './paths';
-import { IGetTimezoneParams, ITimezone } from './types';
+import { ITimezone } from './types';
 
 /**
+ * # Time zones
+ *
  * List all time zones.
  *
+ * @param config - Optional axios request configuration object.
  * @see {@link https://developers.jframework.io/references/api-reference/endpoints/time-zones}
  */
-export const getTimezonesAPI = async (
-    params?: IGetTimezoneParams,
-    config?: AxiosRequestConfig,
-) => {
+export const getTimezones = async (config?: AxiosRequestConfig) => {
     const url = TIMEZONE_PATH.GET_TIMEZONES;
-    
-    const response = await jfwAxios.get<HttpResponse<ITimezone[]>>(url, {
-        ...config,
-        params,
-    });
+    const response = await jfwAxios.get<HttpResponse<ITimezone[]>>(url, config);
+
     return response.data;
 };
--- src/models/timezone/types.ts
+++ src/models/timezone/types.ts
@@ -1,20 +1,62 @@
-import { IdType } from "../base";
+import { DateType, IdType } from '../base';
 
+/**
+ * This class represents the timezone data transfer object.
+ */
 export interface ITimezone {
+    /**
+     * The id of the object.
+     *
+     * @remarks min: 1
+     */
     id: IdType;
+
+    /**
+     * The created date of the object.
+     *
+     * @remarks date-time
+     */
+    createdDate?: DateType | null;
+
+    /**
+     * The timezone code.
+     *
+     * @remarks min: 1
+     */
     code: string;
-    countryCode: string;
+
+    /**
+     * The country code.
+     */
+    countryCode?: string | null;
+
+    /**
+     * The timezone name.
+     *
+     * @remarks min: 1
+     */
     name: string;
+
+    /**
+     * The timezone value.
+     *
+     * @remarks min: 1
+     */
     value: string;
+
+    /**
+     * The timezone display name. Ex: (UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna
+     *
+     * @remarks min: 1
+     */
     displayName: string;
-    description: string;
+
+    /**
+     * The timezone description.
+     */
+    description?: string | null;
 }
 
 //#region API types
-export interface IGetTimezoneParams {
-    keywords?: string;
-    name?: string;
-    countryCode?: string;
-}
 
 //#endregion
