258d1ea2a06c5759da92a48755f3962eed2dfd16
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-06 14:41:27 +0700

    JFW-474 feature: improve wallet api

--- src/models/wallet/api.ts
+++ src/models/wallet/api.ts
@@ -1,95 +1,59 @@
 import { AxiosRequestConfig } from 'axios';
-import { HttpResponseList } from '../../core';
+import { HttpResponse, HttpResponseList } from '../../core';
 import { jfwAxios } from '../../core/client/client';
 import { generatePath } from '../../utils/path';
 import { IdType } from '../base';
 import { WALLET_PATH } from './paths';
 import {
-    IAddMoneyParams,
-    IApplyRedeemParams,
+    IAddMoneyToWalletWithCheckoutLinkParams,
+    IApplyRedeemCodeParams,
     IConvertWalletMoneyParams,
-    ICreateWalletParams,
-    IGetWalletHistoryParams,
-    IGetWalletParams,
+    IGetWalletHistoriesParams,
+    IGetWalletsParams,
     IWallet,
     IWalletHistory,
 } from './types';
 
 /**
- * Gets the wallet information.
+ * # Add money to a Wallet with checkout link
+ *
+ * Generate a checkout link to add money for the wallet.
+ *
+ * @param params - The params for generating a checkout link to add money for the wallet.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/add-money-to-a-wallet-with-checkout-link}
  */
-export const getWalletAPI = async (
-    params: IGetWalletParams,
+export const addMoneyToWalletWithCheckoutLink = async (
+    params: IAddMoneyToWalletWithCheckoutLinkParams,
     config?: AxiosRequestConfig,
-): Promise<IWallet[]> => {
-    const url = WALLET_PATH.GET;
-    const response = await jfwAxios.get(url, { ...config, params });
+) => {
+    const url = WALLET_PATH.ADD_MONEY_TO_WALLET_WITH_CHECKOUT_LINK;
+    const response = await jfwAxios.post<HttpResponse<string>>(url, null, {
+        params,
+        ...config,
+    });
 
     return response.data;
 };
 
 /**
- * Get the wallet histories
- */
-export const getWalletHistoryAPI = async (
-    walletId: IdType,
-    params: IGetWalletHistoryParams,
-    config?: AxiosRequestConfig,
-): Promise<HttpResponseList<IWalletHistory>> => {
-    const url = generatePath(WALLET_PATH.GET_HISTORY, {
-        id: walletId,
-    });
-    const response = await jfwAxios.get(url, { ...config, params });
-
-    return response.data;
-};
-
-// export const getListEarnEventsAPI = async (
-//     params: IGetEarnEventParams,
-//     config?: AxiosRequestConfig,
-// ): Promise<IEarnEvent[]> => {
-//     const url = `${REST_EVENT}`;
-//     const response = await jfwAxios.get(url, { ...config, params });
-
-//     return response.data;
-// };
-
-/**
- * Creates a new wallet by given currency.
- */
-export const createWalletDefaultAPI = async (
-    currencyCode: string,
-    params?: ICreateWalletParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(WALLET_PATH.CREATE_DEFAULT, {
-        currencyCode,
-    });
-
-    return await jfwAxios.post(url, null, { ...config, params });
-};
-
-/**
- * Convert wallet by the given data.
- */
-export const convertWalletMoneyAPI = async (
-    params: IConvertWalletMoneyParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = WALLET_PATH.CONVERT;
-
-    return await jfwAxios.post(url, params, config);
-};
-
-/**
+ * # Apply redeem code
+ *
  * Apply redeem
+ *
+ * This method applies a redeem code to the wallet.
+ * The redeem code must be valid and the wallet must be open.
+ *
+ * @param params - The params for applying redeem code.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/apply-redeem}
  */
-export const applyRedeemAPI = async (
-    params: IApplyRedeemParams,
+export const applyRedeemCode = async (
+    params: IApplyRedeemCodeParams,
     config?: AxiosRequestConfig,
 ) => {
     const { redeemCode, walletId } = params;
-    const url = generatePath(WALLET_PATH.APPLY_REDEEM, {
+    const url = generatePath(WALLET_PATH.APPLY_REDEEM_CODE, {
         walletId,
         redeemCode,
     });
@@ -98,28 +62,121 @@ export const applyRedeemAPI = async (
 };
 
 /**
- * Add money to aWallets with checkout link.
+ * # Create a wallet default
+ *
+ * Create a new wallet by the given currency code.
+ *
+ * @param currencyCode - The currency code. The currency code must be a valid ISO 4217 currency code.
+ * @param testMode - The test mode. By default, it is false.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/create-a-wallet-default}
  */
-export const addMoneyAPI = async (
-    params: IAddMoneyParams,
+export const createWalletDefault = async (
+    currencyCode: string,
+    testMode: boolean = false,
     config?: AxiosRequestConfig,
 ) => {
-    const url = WALLET_PATH.ADD_MONEY_CHECKOUT_LINK;
-
-    return await jfwAxios.post(url, null, {
+    const url = generatePath(WALLET_PATH.CREATE_WALLET_DEFAULT, {
+        currencyCode,
+    });
+    return await jfwAxios.post<HttpResponse<IWallet>>(url, null, {
+        params: {
+            testMode,
+        },
         ...config,
-        params,
     });
 };
 
 /**
- * Close the wallet by the given id
+ * # Convert wallet money
+ *
+ * Convert wallet by the given data.
+ *
+ * @param params - The params for converting wallet money.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/convert-wallet-money}
  */
-export const closeWalletAPI = async (
-    walletId: IdType,
+export const convertWalletMoney = async (
+    params: IConvertWalletMoneyParams,
     config?: AxiosRequestConfig,
 ) => {
-    const url = generatePath(WALLET_PATH.CLOSE, {
-        id: walletId,
+    const url = WALLET_PATH.CONVERT_WALLET_MONEY;
+    const response = await jfwAxios.post<HttpResponse<boolean>>(
+        url,
+        params,
+        config,
+    );
+
+    return response.data;
+};
+
+/**
+ * # Close wallet
+ *
+ * Close a wallet.
+ *
+ * @param id - The id of the wallet.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/close-wallet}
+ */
+export const closeWallet = async (id: IdType, config?: AxiosRequestConfig) => {
+    const url = generatePath(WALLET_PATH.CLOSE_WALLET, {
+        id,
     });
+    const response = await jfwAxios.put<HttpResponse<boolean>>(url, null, {
+        params: {
+            id,
+        },
+        ...config,
+    });
+
+    return response.data;
+};
+
+/**
+ * # Get wallets
+ *
+ * Gets the wallet information.
+ *
+ * @param params - The params for getting wallets.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/get-wallets}
+ */
+export const getWallets = async (
+    params: IGetWalletsParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = WALLET_PATH.GET_WALLETS;
+    const response = await jfwAxios.get<HttpResponse<IWallet[]>>(url, {
+        params,
+        ...config,
+    });
+
+    return response.data;
+};
+
+/**
+ * # Get the wallet histories
+ *
+ * Gets the wallet histories.
+ *
+ * @param id - The id of the wallet.
+ * @param params - The params for getting wallet histories.
+ * @param config - Optional axios request configuration object.
+ * @see {@link https://developers.jframework.io/references/api-reference/endpoints/wallets/get-the-wallet-histories}
+ */
+export const getWalletHistory = async (
+    id: IdType,
+    params: IGetWalletHistoriesParams,
+    config?: AxiosRequestConfig,
+) => {
+    const url = generatePath(WALLET_PATH.GET_WALLET_HISTORIES, {
+        id,
+    });
+    const response = await jfwAxios.get<HttpResponseList<IWalletHistory>>(url, {
+        params,
+        ...config,
+    });
+
+    return response.data;
 };
--- src/models/wallet/paths.ts
+++ src/models/wallet/paths.ts
@@ -1,12 +1,11 @@
 const REST = 'wallets';
 
 export const WALLET_PATH = {
-    GET: `v1/${REST}`,
-    CREATE_DEFAULT: `v1/${REST}/:currencyCode`,
-    GET_HISTORY: `v1/${REST}/:id/histories`,
-    CONVERT: `v1/${REST}/convert`,
-    ADD_MONEY_CHECKOUT_LINK: `v1/${REST}/add-money/checkout-link`,
-    APPLY_REDEEM: `v1/${REST}/:walletId/apply-redeem/:redeemCode`,
-    CLOSE: `v1/${REST}/:id/close`,
-    GET_CONNECTIONS: `v1/${REST}/:userId/connections`,
+    ADD_MONEY_TO_WALLET_WITH_CHECKOUT_LINK: `v1/${REST}/add-money/checkout-link`,
+    APPLY_REDEEM_CODE: `v1/${REST}/:walletId/apply-redeem/{redeemCode}`,
+    CREATE_WALLET_DEFAULT: `v1/${REST}/{currencyCode}`,
+    CONVERT_WALLET_MONEY: `v1/${REST}/convert`,
+    CLOSE_WALLET: `v1/${REST}/{id}/close`,
+    GET_WALLETS: `v1/${REST}`,
+    GET_WALLET_HISTORIES: `v1/${REST}/{id}/histories`,
 };
