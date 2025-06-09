78596edb74f48b65ee11867301ac2f0a38894602
Kenneth <dao.khoi@1bytesoftware.com>
2025-06-09 09:15:10 +0700

    JFW-493 feature: improve HttpStatusCode of JFW

--- src/core/http/constants.ts
+++ src/core/http/constants.ts
@@ -1,10 +1,15 @@
 /**
- * Wait documentation of backend.
- * #JFW-493
+ * # Custom HTTP Codes of Jframework
+ *
+ * @see {@link https://developers.jframework.io/references/api-reference/overview#custom-http-codes}
  */
 export enum JFWHttpStatusCode {
     NotPermission = 209,
+    InvalidUsername = 606,
     WrongPassword = 701,
     UserNotFound = 704,
     EmailExists = 723,
+    UsernameExists = 724,
+    PhoneVerified = 726,
+    OtpExpired = 804,
 }
