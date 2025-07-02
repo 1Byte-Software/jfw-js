80a7fd692d5eaa1ef0d03814e3a0c74c6a19e073
Kenneth <dao.khoi@1bytesoftware.com>
2025-07-02 16:01:52 +0700

    feature: improve Licenses API following developers.jframework.io

--- src/models/license/paths.ts
+++ src/models/license/paths.ts
@@ -3,18 +3,23 @@ const REST_PURCHASE = 'purchase';
 const REST_STATISTIC = 'statistics';
 
 export const LICENSE_PATH = {
+    ACTIVATE_LICENSE: `v1/${REST}/{id}/activate`,
+
     APPLY_LICENSE_TO_LOGGED_USER: `v1/${REST}/apply`,
     APPLY_LICENSE_TO_GIVEN_LOGIN_NAME: `v1/${REST}/apply-to-login-name`,
     APPLY_LICENSE_TO_GIVEN_USER: `v1/${REST}/apply-to-user`,
 
-    CHECK_A_LICENSE_KEY: `v1/${REST}/check`,
+    CHECK_A_LICENSE_CODE: `v1/${REST}/check`,
 
     CREATE_LICENSES: `v1/${REST}`,
     COUNT_LICENSES_CREATED: `v1/${REST}/count`,
+
+    DEACTIVATE_LICENSE: `v1/${REST}/{id}/deactivate`,
+
     DELETE_LICENSE: `v1/${REST}/{id}`,
     GET_LICENSES: `v1/${REST}`,
     GET_LICENSE: `v1/${REST}/{id}`,
-    GENERATE_LICENSE_KEY: `v1/${REST}/generate-key`,
+    GENERATE_LICENSE_CODE: `v1/${REST}/generate-code`,
 
     PURCHASE_TO_ADD_LICENSES_BY_CHECKOUT_LINK: `v1/${REST}/${REST_PURCHASE}/checkout-link`,
     PURCHASE_TO_ADD_LICENSES_BY_WALLET: `v1/${REST}/${REST_PURCHASE}/wallet`,
