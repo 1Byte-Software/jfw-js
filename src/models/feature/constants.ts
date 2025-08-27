cde1ac891d3f5e25616470c485d239001e5299d3
Kenneth <dao.khoi@1bytesoftware.com>
2025-08-27 11:21:28 +0700

    refactor: refactor feature apis

--- src/models/feature/constants.ts
+++ src/models/feature/constants.ts
@@ -1,18 +1,8 @@
-/**
- * Possible values:
- * - `0` - `Function`
- * - `1` - `Limitation`
- */
 export enum FeatureType {
     Function = 'Function',
     Limitation = 'Limitation',
 }
 
-/**
- * Possible values:
- * - `0` - `Inactive`
- * - `1` - `Active`
- */
 export enum FeatureStatus {
     Inactive = 'Inactive',
     Active = 'Active',
