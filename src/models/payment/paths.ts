a68724aa9d09806b89c63101f8bc8c4bf65f4d29
Kenneth <dao.khoi@1bytesoftware.com>
2025-08-27 16:05:13 +0700

    refactor: improve payment APIs

--- src/models/payment/paths.ts
+++ src/models/payment/paths.ts
@@ -1,8 +1,8 @@
 const REST = 'payments';
 
 export const PAYMENT_PATH = {
+    GENERATE_PAYMENT_CHECKOUT_LINK: `v1/${REST}/{id}/generate-checkout-link`,
     GET_PAYMENT: `v1/${REST}/{id}`,
     GET_PAYMENTS: `v1/${REST}`,
     GET_OVERVIEW_SELL_DASHBOARD: `v1/${REST}/dashboard`,
-    GENERATE_PAYMENT_CHECKOUT_LINK: `v1/${REST}/{id}/generate-checkout-link`,
 };
