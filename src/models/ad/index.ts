3fa0f72c1886074795ff5cb71480a9ed2897e4b2
Kenneth <dao.khoi@1bytesoftware.com>
2025-08-29 14:16:28 +0700

    refactor: Refactor export ad model

--- src/models/ad/index.ts
+++ src/models/ad/index.ts
@@ -1,3 +1,23 @@
-export * from './api';
+import {
+    activateAd,
+    createAd,
+    deactivateAd,
+    deleteAd,
+    getAd,
+    getAds,
+    getAdsClientView,
+    updateAd,
+} from './api';
 export * from './constants';
 export * from './types';
+
+export const ad = {
+    activateAd,
+    deactivateAd,
+    createAd,
+    deleteAd,
+    getAdsClientView,
+    getAds,
+    getAd,
+    updateAd,
+};
