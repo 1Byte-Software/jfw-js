22caaa78c5e921f5836efacd015b0f694e9feb86
Kenneth <dao.khoi@1bytesoftware.com>
2025-08-29 14:25:38 +0700

    refactor: Refactor export cdn model

--- src/models/cdn/index.ts
+++ src/models/cdn/index.ts
@@ -1,3 +1,22 @@
-export * from './api';
+import {
+    deleteFileCDN,
+    getFileCDN,
+    getFilesCDN,
+    uploadFile,
+    uploadFileCDN,
+} from './api';
+
 export * from './constants';
 export * from './types';
+
+export const cdn = {
+    deleteFileCDN,
+    getFileCDN,
+    getFilesCDN,
+    uploadFileCDN,
+
+    /**
+     * @deprecated use uploadFileCDN instead
+     */
+    uploadFile,
+};
