ca2449a19d64f2ab6ad8e72ed3b0134559ac07a6
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 10:52:49 +0700

    refactor: Refactor CDNAPI

--- src/models/cdn/index.ts
+++ src/models/cdn/index.ts
@@ -1,22 +1,2 @@
-import {
-    deleteFileCDN,
-    getFileCDN,
-    getFilesCDN,
-    uploadFile,
-    uploadFileCDN,
-} from './api';
-
 export * from './constants';
 export * from './types';
-
-export const cdn = {
-    deleteFileCDN,
-    getFileCDN,
-    getFilesCDN,
-    uploadFileCDN,
-
-    /**
-     * @deprecated use uploadFileCDN instead
-     */
-    uploadFile,
-};
