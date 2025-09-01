f951060cf0bad475d07ff1b633af7536a4496b3c
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:42:14 +0700

    refactor: Refactor StateAPI

--- src/models/state/api.ts
+++ src/models/state/api.ts
@@ -4,17 +4,22 @@ import { jfwAxios } from '../../core/client/client';
 import { STATE_PATH } from './paths';
 import { IState } from './types';
 
-/**
- * # States
- *
- * Get states
- *
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/states}
- */
-export const getStates = async (config?: AxiosRequestConfig) => {
-    const url = STATE_PATH.GET_STATES;
-    const response = await jfwAxios.get<HttpResponse<IState[]>>(url, config);
+export class StateAPI {
+    /**
+     * # States
+     *
+     * Get states
+     *
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/states}
+     */
+    public async getStates(config?: AxiosRequestConfig) {
+        const url = STATE_PATH.GET_STATES;
+        const response = await jfwAxios.get<HttpResponse<IState[]>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
+}
