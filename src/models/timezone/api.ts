2b091fc7b883dcbaf06747de5ce3fdb9b597ddb5
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:49:43 +0700

    refactor: Refactor TimezoneAPI

--- src/models/timezone/api.ts
+++ src/models/timezone/api.ts
@@ -4,17 +4,22 @@ import { jfwAxios } from '../../core/client/client';
 import { TIMEZONE_PATH } from './paths';
 import { ITimezone } from './types';
 
-/**
- * # Time zones
- *
- * List all time zones.
- *
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/time-zones}
- */
-export const getTimezones = async (config?: AxiosRequestConfig) => {
-    const url = TIMEZONE_PATH.GET_TIMEZONES;
-    const response = await jfwAxios.get<HttpResponse<ITimezone[]>>(url, config);
+export class TimezoneAPI {
+    /**
+     * # Time zones
+     *
+     * List all time zones.
+     *
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/time-zones}
+     */
+    public async getTimezones(config?: AxiosRequestConfig) {
+        const url = TIMEZONE_PATH.GET_TIMEZONES;
+        const response = await jfwAxios.get<HttpResponse<ITimezone[]>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
+}
