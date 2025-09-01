15afe73ff21fca56877678c607de9d35fd714f89
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 10:32:40 +0700

    refactor: Refactor AdAPI

--- src/models/ad/api.ts
+++ src/models/ad/api.ts
@@ -7,179 +7,180 @@ import { AD_PATH } from './paths';
 import { IAd, ICreateAdParams, IGetAdsParams, IUpdateAdParams } from './types';
 import { AdPosition } from './constants';
 
-/**
- * # Activate an ad
- *
- * Activate an ad.
- *
- * @param id - The id of the ads to activate.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/activate-an-ad}
- */
-export const activateAd = async (id: IdType, config?: AxiosRequestConfig) => {
-    const url = generatePath(AD_PATH.ACTIVATE_AD, {
-        id,
-    });
-    const response = await jfwAxios.put<HttpResponse<boolean>>(
-        url,
-        null,
-        config,
-    );
+export class AdAPI {
+    /**
+     * # Activate an ad
+     *
+     * Activate an ad.
+     *
+     * @param id - The id of the ads to activate.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/activate-an-ad}
+     */
+    public async activateAd(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(AD_PATH.ACTIVATE_AD, {
+            id,
+        });
+        const response = await jfwAxios.put<HttpResponse<boolean>>(
+            url,
+            null,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Deactivate an ad
- *
- * Deactivate an ad
- *
- * @param id - The id of the ads to deactivate.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/deactivate-an-ad}
- */
-export const deactivateAd = async (id: IdType, config?: AxiosRequestConfig) => {
-    const url = generatePath(AD_PATH.DEACTIVATE_AD, {
-        id,
-    });
-    const response = await jfwAxios.put<HttpResponse<boolean>>(
-        url,
-        null,
-        config,
-    );
+    /**
+     * # Deactivate an ad
+     *
+     * Deactivate an ad
+     *
+     * @param id - The id of the ads to deactivate.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/deactivate-an-ad}
+     */
+    public async deactivateAd(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(AD_PATH.DEACTIVATE_AD, {
+            id,
+        });
+        const response = await jfwAxios.put<HttpResponse<boolean>>(
+            url,
+            null,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Creates an ad
- *
- * Create an ad.
- *
- * @param params - The params for creating an ad.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/create-an-ad}
- */
-export const createAd = async (
-    params: ICreateAdParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = AD_PATH.CREATE_AD;
-    const response = await jfwAxios.post<HttpResponse<boolean>>(
-        url,
-        params,
-        config,
-    );
+    /**
+     * # Creates an ad
+     *
+     * Create an ad.
+     *
+     * @param params - The params for creating an ad.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/create-an-ad}
+     */
+    public async createAd(
+        params: ICreateAdParams,
+        config?: AxiosRequestConfig,
+    ) {
+        const url = AD_PATH.CREATE_AD;
+        const response = await jfwAxios.post<HttpResponse<boolean>>(
+            url,
+            params,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Deletes an ad
- *
- * Deletes an ad
- *
- * @param params - The params for deleting an ad.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/delete-an-ad}
- */
-export const deleteAd = async (id: IdType, config?: AxiosRequestConfig) => {
-    const url = generatePath(AD_PATH.DELETE_AD, {
-        id,
-    });
-    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
+    /**
+     * # Deletes an ad
+     *
+     * Deletes an ad
+     *
+     * @param id - The id of the ad.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/delete-an-ad}
+     */
+    public async deleteAd(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(AD_PATH.DELETE_AD, {
+            id,
+        });
+        const response = await jfwAxios.delete<HttpResponse<boolean>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Gets ads client view
- *
- * Get ads with the client view.
- *
- * @param params - The params for getting ads with the client view.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-ads-client-view}
- */
-export const getAdsClientView = async (
-    position: AdPosition,
-    config?: AxiosRequestConfig,
-) => {
-    const url = AD_PATH.GET_ADS_WITH_CLIENT_VIEW;
+    /**
+     * # Gets ads client view
+     *
+     * Get ads with the client view.
+     *
+     * @param position - The position for getting ads with the client view.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-ads-client-view}
+     */
+    public async getAdsClientView(
+        position: AdPosition,
+        config?: AxiosRequestConfig,
+    ) {
+        const url = AD_PATH.GET_ADS_WITH_CLIENT_VIEW;
 
-    const response = await jfwAxios.get<HttpResponse<IAd[]>>(url, {
-        params: {
-            position,
-        },
-        ...config,
-    });
+        const response = await jfwAxios.get<HttpResponse<IAd[]>>(url, {
+            params: {
+                position,
+            },
+            ...config,
+        });
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Gets ads
- *
- * Get ads.
- *
- * @param params - The params for getting ads.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-ads}
- */
-export const getAds = async (
-    params?: IGetAdsParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = AD_PATH.GET_ADS;
-    const response = await jfwAxios.get<HttpResponseList<IAd>>(url, {
-        ...config,
-        params,
-    });
+    /**
+     * # Gets ads
+     *
+     * Get ads.
+     *
+     * @param params - The params for getting ads.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-ads}
+     */
+    public async getAds(params?: IGetAdsParams, config?: AxiosRequestConfig) {
+        const url = AD_PATH.GET_ADS;
+        const response = await jfwAxios.get<HttpResponseList<IAd>>(url, {
+            ...config,
+            params,
+        });
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Gets an ad
- *
- * Get an ad.
- *
- * @param params - The params for getting an ad.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-an-ad}
- */
-export const getAd = async (id: IdType, config?: AxiosRequestConfig) => {
-    const url = generatePath(AD_PATH.GET_AD, {
-        id,
-    });
-    const response = await jfwAxios.get<HttpResponse<IAd>>(url, config);
+    /**
+     * # Gets an ad
+     *
+     * Get an ad.
+     *
+     * @param id - The id of the ad.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/get-an-ad}
+     */
+    public async getAd(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(AD_PATH.GET_AD, {
+            id,
+        });
+        const response = await jfwAxios.get<HttpResponse<IAd>>(url, config);
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Updates an ad
- *
- * Update an ad
- *
- * @param id - The id of the ads to update.
- * @param params - The params for updating an ad.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/update-an-ad}
- */
-export const updateAd = async (
-    params: IUpdateAdParams,
-    config?: AxiosRequestConfig,
-) => {
-    const { id, ...bodyParams } = params;
-    const url = generatePath(AD_PATH.UPDATE_AD, {
-        id,
-    });
-    const response = await jfwAxios.put<HttpResponse<boolean>>(
-        url,
-        bodyParams,
-        config,
-    );
+    /**
+     * # Updates an ad
+     *
+     * Update an ad
+     *
+     * @param params - The params for updating an ad.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/ads/update-an-ad}
+     */
+    public async updateAd(
+        params: IUpdateAdParams,
+        config?: AxiosRequestConfig,
+    ) {
+        const { id, ...bodyParams } = params;
+        const url = generatePath(AD_PATH.UPDATE_AD, {
+            id,
+        });
+        const response = await jfwAxios.put<HttpResponse<boolean>>(
+            url,
+            bodyParams,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
+}
--- src/models/ad/index.ts
+++ src/models/ad/index.ts
@@ -1,23 +1,2 @@
-import {
-    activateAd,
-    createAd,
-    deactivateAd,
-    deleteAd,
-    getAd,
-    getAds,
-    getAdsClientView,
-    updateAd,
-} from './api';
 export * from './constants';
 export * from './types';
-
-export const ad = {
-    activateAd,
-    deactivateAd,
-    createAd,
-    deleteAd,
-    getAdsClientView,
-    getAds,
-    getAd,
-    updateAd,
-};
