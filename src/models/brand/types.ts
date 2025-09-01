119ceadd924f13fa3aacd343b63a692d44115c88
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 10:49:18 +0700

    refactor: Refactor BrandAPI

--- src/models/brand/types.ts
+++ src/models/brand/types.ts
@@ -2,6 +2,7 @@ import { IPageable, ISortable } from '../../core';
 import { IBaseObject, IdType } from '../base';
 import { IDomain } from '../domain';
 import { BrandStatus } from './constants';
+import { IBrandProfile } from './profile';
 import { IBrandSetting } from './setting';
 
 export interface IBrand extends IBaseObject {
@@ -73,62 +74,6 @@ export interface IBrand extends IBaseObject {
      */
     domains: IDomain[];
 }
-
-export interface IBrandProfile {
-    /**
-     * The slogan.
-     *
-     * @remarks min: 1
-     */
-    slogan: string;
-
-    /**
-     * The logo URL.
-     *
-     * @remarks uri
-     */
-    logoURL?: string | null;
-
-    /**
-     * The favicon URL.
-     *
-     * @remarks uri
-     */
-    faviconURL?: string | null;
-
-    /**
-     * The primary color.
-     *
-     * Color for primary actions, buttons, text links.
-     * 3 and 6 digit hex code values accepted.
-     */
-    primaryColor?: string | null;
-
-    /**
-     * The primary color hover.
-     *
-     * Color for primary actions, buttons, text links hover.
-     * 3 and 6 digit hex code values accepted.
-     */
-    primaryColorHover?: string | null;
-
-    /**
-     * The secondary color of the brand.
-     *
-     * Color for background to accent your UI.
-     * 3 and 6 digit hex code values accepted.
-     */
-    secondaryColor?: string | null;
-
-    /**
-     * The tertiary color of the brand.
-     *
-     * Color for background to accent your UI.
-     * 3 and 6 digit hex code values accepted.
-     */
-    tertiaryColor?: string | null;
-}
-
 //#region API types
 export interface IGetBrandsParams extends IPageable, ISortable {
     /**
