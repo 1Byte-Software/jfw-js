9a1dffaa81615c07e06d8023216b27647927ce13
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:00:37 +0700

    refactor: Refactor CountryAPI

--- src/models/country/api.ts
+++ src/models/country/api.ts
@@ -4,24 +4,26 @@ import { jfwAxios } from '../../core/client/client';
 import { COUNTRY_PATH } from './paths';
 import { ICountry, IGetCountriesParams } from './types';
 
-/**
- * # Countries
- *
- * Get countries
- *
- * @param params - The params for getting countries.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/get-countries}
- */
-export const getCountries = async (
-    params?: IGetCountriesParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = COUNTRY_PATH.GET_COUNTRIES;
-    const response = await jfwAxios.get<HttpResponseList<ICountry>>(url, {
-        params,
-        ...config,
-    });
+export class CountryAPI {
+    /**
+     * # Countries
+     *
+     * Get countries
+     *
+     * @param params - The params for getting countries.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/get-countries}
+     */
+    public async getCountries(
+        params?: IGetCountriesParams,
+        config?: AxiosRequestConfig,
+    ) {
+        const url = COUNTRY_PATH.GET_COUNTRIES;
+        const response = await jfwAxios.get<HttpResponseList<ICountry>>(url, {
+            params,
+            ...config,
+        });
 
-    return response.data;
-};
+        return response.data;
+    }
+}
