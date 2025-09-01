c72480f25e3e9a69eaa4bcbe3c13e5fc830c2ac8
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:20:29 +0700

    refactor: Refactor InvoiceAPI

--- src/models/invoice/api.ts
+++ src/models/invoice/api.ts
@@ -6,78 +6,80 @@ import { IdType } from '../base';
 import { INVOICE_PATH } from './paths';
 import { IGetInvoicesParams, IInvoice } from './types';
 
-/**
- * # Delete an invoice
- *
- * Delete an Invoice.
- *
- * @param id - The id of the invoice.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/delete-an-invoice}
- */
-export const deleteInvoice = async (
-    id: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(INVOICE_PATH.DELETE_INVOICE, {
-        id,
-    });
-    const response = await jfwAxios.delete<HttpResponse<boolean>>(url, config);
+export class InvoiceAPI {
+    /**
+     * # Delete an invoice
+     *
+     * Delete an Invoice.
+     *
+     * @param id - The id of the invoice.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/delete-an-invoice}
+     */
+    public async deleteInvoice(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(INVOICE_PATH.DELETE_INVOICE, {
+            id,
+        });
+        const response = await jfwAxios.delete<HttpResponse<boolean>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Get invoices
- *
- * Get invoices.
- *
- * @param params - The params for getting invoices.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/get-invoices}
- */
-export const getInvoices = async (
-    params?: IGetInvoicesParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = INVOICE_PATH.GET_INVOICES;
-    const response = await jfwAxios.get<HttpResponseList<IInvoice>>(url, {
-        params,
-        ...config,
-    });
+    /**
+     * # Get invoices
+     *
+     * Get invoices.
+     *
+     * @param params - The params for getting invoices.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/get-invoices}
+     */
+    public async getInvoices(
+        params?: IGetInvoicesParams,
+        config?: AxiosRequestConfig,
+    ) {
+        const url = INVOICE_PATH.GET_INVOICES;
+        const response = await jfwAxios.get<HttpResponseList<IInvoice>>(url, {
+            params,
+            ...config,
+        });
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * # Get an invoice
- *
- * Get an invoice.
- *
- * @param id - The id of the invoice.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/get-a-invoice}
- */
-export const getInvoice = async (id: IdType, config?: AxiosRequestConfig) => {
-    const url = generatePath(INVOICE_PATH.GET_INVOICE, {
-        id,
-    });
-    const response = await jfwAxios.get<HttpResponse<IInvoice>>(url, config);
+    /**
+     * # Get an invoice
+     *
+     * Get an invoice.
+     *
+     * @param id - The id of the invoice.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/invoices/get-a-invoice}
+     */
+    public async getInvoice(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(INVOICE_PATH.GET_INVOICE, {
+            id,
+        });
+        const response = await jfwAxios.get<HttpResponse<IInvoice>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * #NOTE: Will update doc in future.
- */
-export const downloadInvoice = async (
-    id: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(INVOICE_PATH.DOWNLOAD_INVOICE, {
-        id,
-    });
-    const response = await jfwAxios.post<Blob>(url, null, config);
+    /**
+     * #NOTE: Will update doc in future.
+     */
+    public async downloadInvoice(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(INVOICE_PATH.DOWNLOAD_INVOICE, {
+            id,
+        });
+        const response = await jfwAxios.post<Blob>(url, null, config);
 
-    return response.data;
-};
+        return response.data;
+    }
+}
