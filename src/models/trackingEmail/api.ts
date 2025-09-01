660574dc7c599007f4ccc447c349e14d3b8416d1
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:51:56 +0700

    refactor: Refactor TrackingEmailAPI

--- src/models/trackingEmail/api.ts
+++ src/models/trackingEmail/api.ts
@@ -6,46 +6,48 @@ import { IdType } from '../base';
 import { TRACKING_EMAIL_PATH } from './paths';
 import { IGetTrackingEmailsParams, ITrackingEmail } from './types';
 
-/**
- * # Get a tracking email
- *
- * Get an tracking email by id.
- *
- * @param id - The id of the tracking email.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-emails/get-a-tracking-email}
- */
-export const getTrackingEmail = async (
-    id: IdType,
-    config?: AxiosRequestConfig,
-) => {
-    const url = generatePath(TRACKING_EMAIL_PATH.GET_EMAIL, {
-        id,
-    });
-    const response = await jfwAxios.get<HttpResponse<ITrackingEmail>>(
-        url,
-        config,
-    );
+export class TrackingEmailAPI {
+    /**
+     * # Get a tracking email
+     *
+     * Get an tracking email by id.
+     *
+     * @param id - The id of the tracking email.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-emails/get-a-tracking-email}
+     */
+    public async getTrackingEmail(id: IdType, config?: AxiosRequestConfig) {
+        const url = generatePath(TRACKING_EMAIL_PATH.GET_EMAIL, {
+            id,
+        });
+        const response = await jfwAxios.get<HttpResponse<ITrackingEmail>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
 
-/**
- * Get the list of tracking emails.
- *
- * @param params - The params for getting the list of tracking emails.
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-emails/get-tracking-emails}
- */
-export const getTrackingEmails = async (
-    params?: IGetTrackingEmailsParams,
-    config?: AxiosRequestConfig,
-) => {
-    const url = TRACKING_EMAIL_PATH.GET_EMAILS;
-    const response = await jfwAxios.get<HttpResponseList<ITrackingEmail>>(url, {
-        params,
-        ...config,
-    });
+    /**
+     * Get the list of tracking emails.
+     *
+     * @param params - The params for getting the list of tracking emails.
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/tracking-emails/get-tracking-emails}
+     */
+    public async getTrackingEmails(
+        params?: IGetTrackingEmailsParams,
+        config?: AxiosRequestConfig,
+    ) {
+        const url = TRACKING_EMAIL_PATH.GET_EMAILS;
+        const response = await jfwAxios.get<HttpResponseList<ITrackingEmail>>(
+            url,
+            {
+                params,
+                ...config,
+            },
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
+}
