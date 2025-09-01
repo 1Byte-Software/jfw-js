622b28030b6c3834e34502a3d4abb83811bc260d
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-01 11:23:54 +0700

    refactor: Refactor LanguageAPI

--- src/models/language/api.ts
+++ src/models/language/api.ts
@@ -4,15 +4,20 @@ import { jfwAxios } from '../../core/client/client';
 import { LANGUAGE_PATH } from './paths';
 import { ILanguage } from './types';
 
-/**
- * # Gets all languages.
- *
- * @param config - Optional axios request configuration object.
- * @see {@link https://developers.jframework.io/references/api-reference/endpoints/languages}
- */
-export const getLanguages = async (config?: AxiosRequestConfig) => {
-    const url = LANGUAGE_PATH.GET_LANGUAGES;
-    const response = await jfwAxios.get<HttpResponse<ILanguage[]>>(url, config);
+export class LanguageAPI {
+    /**
+     * # Gets all languages.
+     *
+     * @param config - Optional axios request configuration object.
+     * @see {@link https://developers.jframework.io/references/api-reference/endpoints/languages}
+     */
+    public async getLanguages(config?: AxiosRequestConfig) {
+        const url = LANGUAGE_PATH.GET_LANGUAGES;
+        const response = await jfwAxios.get<HttpResponse<ILanguage[]>>(
+            url,
+            config,
+        );
 
-    return response.data;
-};
+        return response.data;
+    }
+}
