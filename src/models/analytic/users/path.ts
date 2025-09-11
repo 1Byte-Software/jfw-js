037f30236320b1c266050a5f8b90b7557ed737a0
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-11 13:29:31 +0700

    feature: add getDailyUserAnalytics and getUserGeographyAnalytics api

--- src/models/analytic/users/path.ts
+++ src/models/analytic/users/path.ts
@@ -4,4 +4,6 @@ const REST_USER = 'users';
 
 export const ANALYTIC_USER_PATH = {
     GET_SUMMARY_USERS: `v1/${REST_ANALYTIC}/${REST_USER}/summary`,
+    GET_DAILY_USERS_ANALYTICS: `v1/${REST_ANALYTIC}/${REST_USER}/daily`,
+    GET_USER_GEOGRAPHY_ANALYTICS: `v1/${REST_ANALYTIC}/${REST_USER}/geography`,
 };
