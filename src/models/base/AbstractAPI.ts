edbf9a0dd2b3a4994c2e7269a19e01d9c50bb623
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-12 15:02:58 +0700

    feat: add data param for callAPI function

--- src/models/base/AbstractAPI.ts
+++ src/models/base/AbstractAPI.ts
@@ -5,17 +5,19 @@ export interface CallAPIOptions {
     method?: Method;
     path: string;
     pathParams?: Record<string, string>;
+    data?: AxiosRequestConfig['data'];
 }
 
 export abstract class AbstractAPI {
     constructor(protected axios: Axios) {}
 
     protected async callAPI<T = any>(options: CallAPIOptions) {
-        const { method = 'GET', path, pathParams = {} } = options;
+        const { method = 'GET', path, pathParams = {}, data = null } = options;
 
         const axiosConfig: AxiosRequestConfig = {
             method,
             url: generatePath(path, pathParams),
+            data,
         };
 
         const response = await this.axios.request<T>(axiosConfig);
