cbd8a5a0735b8bd2aae9ba019e54133b2c13ae58
Kenneth <dao.khoi@1bytesoftware.com>
2025-11-10 16:20:08 +0700

    feat: add get current brand links by type

--- src/features/domain/brandLinks/paths.ts
+++ src/features/domain/brandLinks/paths.ts
@@ -7,6 +7,7 @@ export const BRAND_LINK_PATH = {
     DELETE_BRAND_LINK: `v1/${REST_DOMAIN}/{domainId}/${REST_BRAND_LINKS}/{brandLinkId}`,
     GET_BRAND_LINK: `v1/${REST_DOMAIN}/{domainId}/${REST_BRAND_LINKS}/{brandLinkId}`,
     GET_BRAND_LINKS_BY_TYPE: `v1/${REST_DOMAIN}/{domainId}/${REST_BRAND_LINKS}/load-type/{type}`,
+    GET_CURRENT_BRAND_LINKS_BY_TYPE: `v1/${REST_DOMAIN}/current/${REST_BRAND_LINKS}/load-type/{type}`,
     GET_BRAND_LINKS: `v1/${REST_DOMAIN}/{domainId}/${REST_BRAND_LINKS}`,
     UPDATE_BRAND_LINK: `v1/${REST_DOMAIN}/{domainId}/${REST_BRAND_LINKS}/{brandLinkId}`,
 };
